994529f47baaa4a4433c3c122fed8a51b26b18af
leon-do <leondo123@gmail.com>
2021-06-07 11:44:06 -0400

    update host url

--- Web3Unity/Scripts/Scenes/_Config.cs
+++ Web3Unity/Scripts/Scenes/_Config.cs
@@ -10,7 +10,7 @@ public class _Config : MonoBehaviour
     public static string Account = "0x0000000000000000000000000000000000000001";
 
     // host url for API
-    public readonly static string Host = "https://restx-ethereum.herokuapp.com";
+    public readonly static string Host = "http://3.16.155.209";
 
     void Awake()
     {
