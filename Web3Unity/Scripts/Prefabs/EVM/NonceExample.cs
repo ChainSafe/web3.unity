2f64adf3090655df459bec57a60af4f0a0a697bf
leon-do <leondo123@gmail.com>
2021-07-21 20:49:19 -0400

    format code

--- Web3Unity/Scripts/Prefabs/EVM/NonceExample.cs
+++ Web3Unity/Scripts/Prefabs/EVM/NonceExample.cs
@@ -9,6 +9,7 @@ public class NonceExample : MonoBehaviour
         string chain = "ethereum";
         string network = "rinkeby";
         string account = "0xdD4c825203f97984e7867F11eeCc813A036089D1";
+
         string nonce = await EVM.Nonce(chain, network, account);
         print(nonce);
     }
