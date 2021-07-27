3efbf7cf8957e2dc3ebc262eb36dafbfb47565a5
leon-do <leondo123@gmail.com>
2021-07-26 23:15:13 -0400

    add bsc in comments

--- WebGLTemplates/Web3GL-2020/network.js
+++ WebGLTemplates/Web3GL-2020/network.js
@@ -6,9 +6,14 @@ Used to set the network
 4 Rinkeby
 5 Goerli
 42 Kovan
+56 Binance Smart Chain Mainnet
+97 Binance Smart Chain Testnet
 100 xDai
-80001 Matic Testnet
 137 Matic
+1287 Moonbase Testnet
+80001 Matic Testnet
+43113 Avalanche Testnet
+43114 Avalanche Mainnet
 */
 
 window.web3NetworkId = 1;
