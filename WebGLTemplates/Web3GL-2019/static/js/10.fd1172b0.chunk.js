8ec6bbdc6ab2320ca0c16a329aad2b67d0df6a72
leon-do <leondo123@gmail.com>
2021-08-02 11:52:08 -0400

    bump web3gl wallet version

--- WebGLTemplates/Web3GL-2019/static/js/10.fd1172b0.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpweb3gl=this.webpackJsonpweb3gl||[]).push([[10],{846:function(p,s){}}]);
-//# sourceMappingURL=10.fd1172b0.chunk.js.map
---
+++ WebGLTemplates/Web3GL-2019/static/js/83.5ea78171.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpweb3gl=this.webpackJsonpweb3gl||[]).push([[83],{1478:function(p,s){}}]);
+//# sourceMappingURL=83.5ea78171.chunk.js.map
