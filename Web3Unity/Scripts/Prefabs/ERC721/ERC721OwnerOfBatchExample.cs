9c3a2371f71091d270a2fdf462d0168d51243a29
leon-do <leondo123@gmail.com>
2021-08-07 23:28:56 -0400

    ERC721OwnerOfBatchExample

--- Web3Unity/Scripts/Prefabs/ERC721/ERC721OwnerOfBatchExample.cs
+++ Web3Unity/Scripts/Prefabs/ERC721/ERC721OwnerOfBatchExample.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ERC721BalanceOfBatchExample : MonoBehaviour
+public class ERC721OwnerOfBatchExample : MonoBehaviour
 {
     async void Start()
     {
