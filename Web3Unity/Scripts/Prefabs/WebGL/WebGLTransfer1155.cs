fa961340b5cc1907c4f5b99de2608d2654c7868c
leon-do <leondo123@gmail.com>
2021-09-07 23:11:52 -0400

    webgl send with gas limit

--- Web3Unity/Scripts/Prefabs/WebGL/WebGLTransfer1155.cs
+++ Web3Unity/Scripts/Prefabs/WebGL/WebGLTransfer1155.cs
@@ -26,9 +26,11 @@ public class WebGLTransfer1155: MonoBehaviour
         string args = JsonConvert.SerializeObject(obj);
         // value in wei
         string value = "0";
+        // gas limit OPTIONAL
+        string gas = "21000";
         // connects to user's browser wallet (metamask) to send a transaction
         try {
-            string response = await Web3GL.Send(method, abi, contract, args, value);
+            string response = await Web3GL.Send(method, abi, contract, args, value, gas);
             Debug.Log(response);
         } catch (Exception e) {
             Debug.LogException(e, this);
