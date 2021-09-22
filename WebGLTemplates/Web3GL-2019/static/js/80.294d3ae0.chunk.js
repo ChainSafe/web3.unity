e9bf996b13d435fe38674bfc307b75b5cbed7115
leon-do <leondo123@gmail.com>
2021-09-22 13:54:20 -0400

    minify WebGLTemplate

--- WebGLTemplates/Web3GL-2019/static/js/80.294d3ae0.chunk.js
+++ WebGLTemplates/Web3GL-2019/static/js/80.294d3ae0.chunk.js
@@ -1,2 +1,1 @@
 (this.webpackJsonpweb3gl=this.webpackJsonpweb3gl||[]).push([[80],{1135:function(p,s){}}]);
-//# sourceMappingURL=80.294d3ae0.chunk.js.map
--- WebGLTemplates/Web3GL-2020/static/js/82.7130bb3a.chunk.js
+++ WebGLTemplates/Web3GL-2020/static/js/82.7130bb3a.chunk.js
@@ -1,2 +1,1 @@
 (this.webpackJsonpweb3gl=this.webpackJsonpweb3gl||[]).push([[82,19],{789:function(p,s){}}]);
-//# sourceMappingURL=82.7130bb3a.chunk.js.map
