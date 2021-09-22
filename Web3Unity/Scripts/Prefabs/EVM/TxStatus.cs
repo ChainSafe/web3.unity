a99ce3435ed01de5c4aae9e2af1890004c4e3929
leon-do <leondo123@gmail.com>
2021-09-22 11:58:09 -0400

    EVM.TxStatus

--- Web3Unity/Scripts/Prefabs/EVM/TxStatus.cs
+++ Web3Unity/Scripts/Prefabs/EVM/TxStatus.cs
@@ -8,7 +8,7 @@ public class TxStatus: MonoBehaviour
     {
         string chain = "ethereum";
         string network = "mainnet";
-        string transaction = "0x911d4ec9193e0dc14d9d034d88c311453112c5097f29c366ccc9c5e5bc7072e11";
+        string transaction = "0x911d4ec9193e0dc14d9d034d88c311453112c5097f29c366ccc9c5e5bc7072e1";
 
         string txStatus = await EVM.TxStatus(chain, network, transaction);
         print(txStatus); // success, fail, pending
