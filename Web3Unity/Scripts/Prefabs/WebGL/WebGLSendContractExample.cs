899c1013be9d648f4c3efc83b3ae142b263ec963
leon-do <leondo123@gmail.com>
2021-10-04 20:05:46 -0400

    Update WebGLSendContractExample.cs and CustomCallExample.cs

--- Web3Unity/Scripts/Prefabs/WebGL/WebGLSendContractExample.cs
+++ Web3Unity/Scripts/Prefabs/WebGL/WebGLSendContractExample.cs
@@ -8,17 +8,17 @@ public class WebGLSendContractExample : MonoBehaviour
     async public void OnSendContract()
     {
         // smart contract method to call
-        string method = "increment";
+        string method = "addTotal";
         // abi in json format
-        string abi = "[ { \"inputs\": [], \"name\": \"increment\", \"outputs\": [], \"stateMutability\": \"nonpayable\", \"type\": \"function\" }, { \"inputs\": [], \"name\": \"x\", \"outputs\": [ { \"internalType\": \"uint256\", \"name\": \"\", \"type\": \"uint256\" } ], \"stateMutability\": \"view\", \"type\": \"function\" } ]";
+        string abi = "[ { \"inputs\": [ { \"internalType\": \"uint8\", \"name\": \"_myArg\", \"type\": \"uint8\" } ], \"name\": \"addTotal\", \"outputs\": [], \"stateMutability\": \"nonpayable\", \"type\": \"function\" }, { \"inputs\": [], \"name\": \"myTotal\", \"outputs\": [ { \"internalType\": \"uint256\", \"name\": \"\", \"type\": \"uint256\" } ], \"stateMutability\": \"view\", \"type\": \"function\" } ]";
         // address of contract
-        string contract = "0xB6B8bB1e16A6F73f7078108538979336B9B7341C";
+        string contract = "0x7286Cf0F6E80014ea75Dbc25F545A3be90F4904F";
         // array of arguments for contract
-        string args = "[]";
+        string args = "[\"1\"]";
         // value in wei
         string value = "0";
         // gas limit OPTIONAL
-        string gas = "21000";
+        string gas = "";
         // connects to user's browser wallet (metamask) to update contract state
         try {
             string response = await Web3GL.SendContract(method, abi, contract, args, value, gas);
