b5b9c96e63dd887a705ba35b7a6e73732e3961dc
Leon Do <leon@leons-mbp.lan>
2021-11-13 20:35:52 -0500

    update comments

--- Web3Unity/Scripts/Prefabs/ERC721/ERC721OwnerOfBatchExample.cs
+++ Web3Unity/Scripts/Prefabs/ERC721/ERC721OwnerOfBatchExample.cs
@@ -10,8 +10,8 @@ public class ERC721OwnerOfBatchExample : MonoBehaviour
         string network = "mainnet";
         string contract = "0xA74E199990FF572A320508547Ab7f44EA51e6F28";
         string[] tokenIds = {"700", "791"};
-        string multicall = ""; // optional. your multicall contract: https://github.com/makerdao/multicall
-        string rpc = ""; // your custom rpc
+        string multicall = ""; // optional: multicall contract https://github.com/makerdao/multicall
+        string rpc = ""; // optional: custom rpc
 
         List<string> batchOwners = await ERC721.OwnerOfBatch(chain, network, contract, tokenIds, multicall, rpc);
         foreach (string owner in batchOwners)
