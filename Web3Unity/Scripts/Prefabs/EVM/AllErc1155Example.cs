cc12eeb7c109bb60bf1823dfc7991dc84b46ded7
kwame <kwame.bryan@gmail.com>
2022-02-18 19:15:42 -0500

    updated prefab examples.

--- Web3Unity/Scripts/Prefabs/EVM/AllErc1155Example.cs
+++ Web3Unity/Scripts/Prefabs/EVM/AllErc1155Example.cs
@@ -19,6 +19,8 @@ public class AllErc1155Example : MonoBehaviour
         string network = "rinkeby"; // mainnet ropsten kovan rinkeby goerli
         string account = "0xebc0e6232fb9d494060acf580105108444f7c696";
         string contract = "";
+        int first = 500;
+        int skip = 0;
         string response = await EVM.AllErc1155(chain, network, account, contract);
         try
         {
