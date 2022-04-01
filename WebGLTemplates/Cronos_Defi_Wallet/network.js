b060a59e1242a6596c7970e803e5ec16ae60305a
kwame <kwame.bryan@gmail.com>
2022-04-01 09:39:24 -0400

    Added Cronos WebTemplate  / Documentation
    Adding Cronos WebTemplate and Documentation

---
+++ WebGLTemplates/Cronos_Defi_Wallet/network.js
@@ -0,0 +1,25 @@
+/*
+Used to set the network: https://chainlist.org/
+1 Mainnet
+3 Ropsten
+4 Rinkeby
+5 Goerli
+42 Kovan
+56 Binance Smart Chain Mainnet
+97 Binance Smart Chain Testnet
+100 xDai
+137 Matic
+1287 Moonbase Testnet
+80001 Matic Testnet
+43113 Avalanche Testnet
+43114 Avalanche Mainnet
+42220 Celo Mainnet
+44787 Celo Alfajores Testnet
+62320 Celo Baklava Testnet
+1666700000 Harmony Testnet Shard0
+1666600000 Harmony Mainnet Shard0
+25 Cronos Mainnet Beta
+338 Cronos Testnet
+*/
+
+window.web3ChainId = 1;
