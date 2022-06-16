f34ed4ddf643d02ab057122a1adb9cedeeac544a
Yann Guineau <yann@chainsafe.io>
2022-06-16 11:02:14 -0400

    Revert "Use IPFS upload in each minter prefab"
    This reverts commit 667c556c1d4786b79c068fb5311566a55b9339c9.

--- Web3Unity/Scripts/Prefabs/Minter/MintWeb3Wallet.cs
+++ Web3Unity/Scripts/Prefabs/Minter/MintWeb3Wallet.cs
@@ -10,19 +10,13 @@ public class MintWeb3Wallet : MonoBehaviour
     public string network = "rinkeby"; // mainnet ropsten kovan rinkeby goerli
     public string account = PlayerPrefs.GetString("Account");
     public string to = "0x7259E32e35cf880aEACfbD412E7F4Baa8606e04c";
+    public string cid = "QmXjWjjMU8r39UCEZ8483aNedwNRFRLvvV9kwq1GpCgthj";
     public string chainId = "4";
 
     
     // Start is called before the first frame update
     public async void MintNFT()
     {
-        var data = System.Text.Encoding.UTF8.GetBytes("YOUR_DATA");
-        
-        IPFS ipfs = new IPFS("YOUR_CHAINSAFE_STORAGE_API_KEY");
-        string cid  = await ipfs.Upload("BUCKET_ID", "/PATH", "FILENAME.ext", data, "application/octet-stream");
-
-        Debug.Log("IPFS CID: " + cid);
-        
         CreateMintModel.Response nftResponse = await EVM.CreateMint(chain, network, account, to, cid);
         Debug.Log("NFT Response: " + nftResponse);
         // connects to user's browser wallet (metamask) to send a transaction
