a7e92c3ebb62395f9bafcd7f5350fda80457b15c
kwame <kwame.bryan@gmail.com>
2022-07-05 18:18:49 -0400

    Sha3Keccack().CalculateHash(_message)

--- Web3Unity/Scripts/Library/Web3Wallet.cs
+++ Web3Unity/Scripts/Library/Web3Wallet.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
+using Nethereum.Hex.HexConvertors.Extensions;
+using Nethereum.Signer;
 using UnityEngine;
 using Nethereum.Util;
 
@@ -63,10 +65,9 @@ public class Web3Wallet
 
     public static string Sha3(string _message)
     {
-       
-        Sha3Keccack hasher = new Sha3Keccack();
-        string hash = hasher.CalculateHash(_message);
-        // 0x1c8aff950685c2ed4bc3174f3472287b56d9517b9c948127319a09a7a36deac8
-        return "0x" + hash;
+        var signer = new EthereumMessageSigner();
+        var hash = new Sha3Keccack().CalculateHash(_message).EnsureHexPrefix();
+        // 0x06b3dfaec148fb1bb2b066f10ec285e7c9bf402ab32aa78a5d38e34566810cd2
+        return  hash;
     }
 }
