63ca5a67f0c594f1435a8004e4f4fe6d2731da55
sneakzttv <sneakz420gaming@gmail.com>
2022-07-22 16:09:40 +0800

    webgl changes

--- Web3Unity/Scripts/Prefabs/Minter/GetVoucherWebGL1155.cs
+++ Web3Unity/Scripts/Prefabs/Minter/GetVoucherWebGL1155.cs
@@ -1,11 +1,12 @@
-﻿using System;
+using System;
 using Models;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GetVoucherWeb3Wallet1155 : MonoBehaviour
+#if UNITY_WEBGL
+public class GetVoucherWebGL1155 : MonoBehaviour
 {
-    public async void Get1155VoucherButton()
+     public async void Get1155VoucherButton()
     {
         var voucherResponse1155 = await EVM.Get1155Voucher();
         Debug.Log("Voucher Response 1155 Signature: " + voucherResponse1155.signature);
@@ -14,6 +15,7 @@ public class GetVoucherWeb3Wallet1155 : MonoBehaviour
         Debug.Log("Voucher Response 1155 Nonce: " + voucherResponse1155.nonce);
         Debug.Log("Voucher Response 1155 Signer: " + voucherResponse1155.signer);
         // saves the voucher to player prefs, you can change this if you like to fit your system
-        PlayerPrefs.SetString("Web3Voucher1155", voucherResponse1155.signer);
+        PlayerPrefs.SetString("WebGLVoucher1155", voucherResponse1155.signer); 
     }
 }
+#endif
