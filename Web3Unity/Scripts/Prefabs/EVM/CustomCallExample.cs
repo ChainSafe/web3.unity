6aef0e24d5f04629494a55bf2cb017b49ee7cf31
kwame <kwame.bryan@gmail.com>
2022-08-04 20:57:30 -0400

    Revert "Merge pull request #205 from ChainSafe/redeem-and-auth-prefabs"
    This reverts commit 6a7c9f3abb2728541b0723305c5b4b2d9d975d90, reversing
    changes made to 6d74b3ad6ff07a71625ed7542313fe8f66ec3d36.

--- Web3Unity/Scripts/Prefabs/EVM/CustomCallExample.cs
+++ Web3Unity/Scripts/Prefabs/EVM/CustomCallExample.cs
@@ -1,23 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
-using Newtonsoft.Json;
 using UnityEngine;
 
 public class CustomCallExample : MonoBehaviour
 {
-
-    // set chain: ethereum, moonbeam, polygon etc
-    string chain = "ethereum";
-    // set network mainnet, testnet
-    string network = "goerli";
-    // abi in json format
-    string abi = "[ { \"inputs\": [ { \"internalType\": \"uint8\", \"name\": \"_myArg\", \"type\": \"uint8\" } ], \"name\": \"addTotal\", \"outputs\": [], \"stateMutability\": \"nonpayable\", \"type\": \"function\" }, { \"inputs\": [], \"name\": \"myTotal\", \"outputs\": [ { \"internalType\": \"uint256\", \"name\": \"\", \"type\": \"uint256\" } ], \"stateMutability\": \"view\", \"type\": \"function\" } ]";
-    // address of contract
-    string contract = "0x741C3F3146304Aaf5200317cbEc0265aB728FE07";
-
-
-    /*
-        //Solidity Contract
+    async void Start()
+    {
+        /*
         // SPDX-License-Identifier: MIT
         pragma solidity ^0.8.0;
 
@@ -29,20 +18,21 @@ public class CustomCallExample : MonoBehaviour
             }
         }
         */
-    
-    public void Start()
-    {
-        CheckVariable();
-    }
-
-    async public void CheckVariable()
-    {
+        // set chain: ethereum, moonbeam, polygon etc
+        string chain = "ethereum";
+        // set network mainnet, testnet
+        string network = "rinkeby";
+        // smart contract method to call
         string method = "myTotal";
+        // abi in json format
+        string abi = "[ { \"inputs\": [ { \"internalType\": \"uint8\", \"name\": \"_myArg\", \"type\": \"uint8\" } ], \"name\": \"addTotal\", \"outputs\": [], \"stateMutability\": \"nonpayable\", \"type\": \"function\" }, { \"inputs\": [], \"name\": \"myTotal\", \"outputs\": [ { \"internalType\": \"uint256\", \"name\": \"\", \"type\": \"uint256\" } ], \"stateMutability\": \"view\", \"type\": \"function\" } ]";
+        // address of contract
+        string contract = "0x7286Cf0F6E80014ea75Dbc25F545A3be90F4904F";
         // array of arguments for contract
         string args = "[]";
         // connects to user's browser wallet to call a transaction
         string response = await EVM.Call(chain, network, contract, abi, method, args);
         // display response in game
-        print("Contract Variable Total: " + response);
+        print(response);
     }
 }
