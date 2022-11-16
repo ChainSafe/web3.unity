11cd1332faeb9c5354783406970f5560f17a7272
sneakzttv <57473220+sneakzttv@users.noreply.github.com>
2022-11-17 03:03:32 +0800

    Mint alterations
    Minting without a voucher removed.
    Chicken demo example added with the use of arrays/tuples

--- Web3Unity/Scripts/Prefabs/Minter/MintWeb3Wallet721.cs
+++ Web3Unity/Scripts/Prefabs/Minter/MintWeb3Wallet721.cs
@@ -20,22 +20,6 @@ namespace Web3Unity.Scripts.Prefabs.Minter
             account = PlayerPrefs.GetString("Account");
         }
 
-        // Start is called before the first frame update
-        public async void MintNft721()
-        {
-            CreateMintModel.Response nftResponse = await EVM.CreateMint(chain, network, account, to, cid721, type721);
-            Debug.Log("NFT Response: " + nftResponse);
-            // connects to user's browser wallet (metamask) to send a transaction
-            try
-            {
-                string response = await Web3Wallet.SendTransaction(chainId, nftResponse.tx.to, nftResponse.tx.value, nftResponse.tx.data, nftResponse.tx.gasLimit, nftResponse.tx.gasPrice);
-                print(response);
-                Debug.Log(response);
-            } catch (Exception e) {
-                Debug.LogException(e, this);
-            }
-        }
-
         public async void VoucherMintNft721()
         {
             // validates the account that sent the voucher, you can change this if you like to fit your system
