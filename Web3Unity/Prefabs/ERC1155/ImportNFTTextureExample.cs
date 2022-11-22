1dc1889176cf952f0457610572ffb236a026fc94
sneakzttv <57473220+sneakzttv@users.noreply.github.com>
2022-11-23 05:16:19 +0800

    1.6.0 fixes
    1.6.0 fixes

--- Web3Unity/Prefabs/ERC1155/ImportNFTTextureExample.cs
+++ Web3Unity/Prefabs/ERC1155/ImportNFTTextureExample.cs
@@ -9,8 +9,6 @@ public class ImportNFTTextureExample : MonoBehaviour
     public class Response {
         public string image;
     }
-
-    
     async void Start()
     {
         string chain = "ethereum";
