a8569120abdfd8689df42e8f4e520b0238f1a05a
kwame <kwame.bryan@gmail.com>
2022-11-30 15:31:34 -0500

    Show Registration
    Show's registration if one hasn't been created already.

--- Plugins/GameRegistration.cs
+++ Plugins/GameRegistration.cs
@@ -9,6 +9,7 @@ public class Startup
     {
         if (PlayerPrefs.GetString("Registered") == "")
         {
+            EditorApplication.ExecuteMenuItem("Window/ChainSafeServerSettings");
             Debug.LogError("ProjectID Not Valid! Please Go To Dashboard.Gaming.Chainsafe.io To Get A New ProjectID & Enter It By Pressing Window -> ChainsafeServerSettings");
         }
     }
