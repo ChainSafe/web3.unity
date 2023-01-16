c6ef50deadd19cf1419076576e5600bfa0ee3535
sneakzttv <57473220+sneakzttv@users.noreply.github.com>
2023-01-17 06:31:48 +0800

    all erc fix
    all erc fix, graphless

---
+++ Web3Unity/Scripts/Prefabs/EVM/AllErc721.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class AllErc721 : MonoBehaviour
+{
+    string account;
+    int balanceSearched;
+    public string chain;
+    public string network;
+    public int amountOfTokenIdsToSearch;
+    public string[] nftContracts = {};
+
+    async void Start()
+    {
+        // This is the account taken from the user login scene
+        account = PlayerPrefs.GetString("Account");
+        // Searches through your listed contracts
+        foreach (string contract in nftContracts)
+        {
+            int balance = await ERC721.BalanceOf(chain, network, contract, account);
+            Debug.Log("Balance of contract " + contract + ": " + balance);
+            // if i is less than the selected amount of tokenIDs to search, keep searching
+            for (int i = 1; i <amountOfTokenIdsToSearch; i++)
+            {
+                // if balanceSearched is less than the balance for each contract, keep searching
+                if (balanceSearched < balance)
+                {
+                    string ownerOf = await ERC721.OwnerOf(chain, network, contract, i.ToString());
+                    // if token id id matches the account from login, print the tokenID and get the URI
+                    if (ownerOf == account)
+                    {
+                        string uri = await ERC1155.URI(chain, network, contract, i.ToString());
+                        Debug.Log("TokenID: " + i);
+                        Debug.Log("Token URI: " + uri);
+                        balance ++;
+                    }
+                }
+            }
+        }
+    }
+}
