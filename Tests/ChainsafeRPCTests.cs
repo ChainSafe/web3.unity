63d2aeac646ee3d36a4c3a7f85d0bcb2156bb044
Juan Manuel Spoleti <104365141+juans-chainsafe@users.noreply.github.com>
2023-03-02 11:11:46 -0300

    chainsafeRPC test as example

---
+++ Tests/ChainsafeRPCTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using Web3Unity.Scripts.Library.Ethers.Providers;
+
+namespace Tests
+{
+    [TestFixture]
+    public class ChainSafeRpcTests
+    {
+        private JsonRpcProvider _provider;
+        [SetUp]
+        public void SetUp()
+        {
+            _provider = new JsonRpcProvider("https://goerli.infura.io/v3/904006115c764661965dc0909e5ed473");
+        }
+        
+        [Test]
+        public void GetNetworkTest()
+        {
+            var network = _provider.GetNetwork().Result;
+            Assert.AreEqual(network.Name, "Goerli");
+            Assert.AreEqual(network.ChainId, 5);
+        }
+    }
+}
