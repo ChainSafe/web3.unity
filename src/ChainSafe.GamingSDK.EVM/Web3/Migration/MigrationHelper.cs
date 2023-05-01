ffd5e55062465ee38bec745568ad65a4b6fd430f
creeppak <oleksandr@chainsafe.io>
2023-05-01 12:18:29 +0300

    Web3.Core moved to Web3/Core folder

---
+++ src/ChainSafe.GamingSDK.EVM/Web3/Migration/MigrationHelper.cs
@@ -0,0 +1,20 @@
+﻿using ChainSafe.GamingWeb3.Build;
+using ChainSafe.GamingWeb3.Evm;
+using ChainSafe.GamingWeb3.Evm.Providers;
+
+namespace ChainSafe.GamingWeb3.Migration
+{
+  public static class MigrationHelper
+  {
+    public static JsonRpcProvider BuildJsonRpcProvider(string url, Network network)
+    {
+      var web3 = new Web3Builder()
+        .Configure(services =>
+        {
+          // todo
+        }).Build();
+
+      return (JsonRpcProvider)web3.Provider;
+    }
+  }
+}
