3f7b4f01b3f7f2b0df45cedc07e631af634a1849
Arshia Ghafoori <arshia001@live.com>
2023-05-03 15:21:17 +0400

    Add code analysis to EVM.Tests

--- src/ChainSafe.GamingSDK.EVM.Tests/Fixture.cs
+++ src/ChainSafe.GamingSDK.EVM.Tests/Fixture.cs
@@ -1,9 +1,4 @@
 ﻿using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Web3Unity.Scripts.Library.Ethers.NetCore;
 
 namespace ChainSafe.GamingSDK.EVM.Tests
