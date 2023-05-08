f7f3869a85988f428d6478f03a0b58544656d5d5
kwame <kwame.bryan@gmail.com>
2023-05-08 09:19:17 -0400

    Update index.js
    remove wallet connect 1.x

--- Assets/WebGLTemplates/Web3GL-MetaMask/web3/index.js
+++ Assets/WebGLTemplates/Web3GL-MetaMask/web3/index.js
@@ -7,8 +7,6 @@ document.body.appendChild(Object.assign(document.createElement("script"), { type
 
 // uncomment to enable torus wallet
 // document.body.appendChild(Object.assign(document.createElement("script"), { type: "text/javascript", src: "https://unpkg.com/@toruslabs/torus-embed" }));
-// uncomment to enable walletconnect
-// document.body.appendChild(Object.assign(document.createElement("script"), { type: "text/javascript", src: "https://unpkg.com/@walletconnect/web3-provider@1.2.1/dist/umd/index.min.js" }));
 
 // load web3gl to connect to unity
 window.web3gl = {
@@ -47,12 +45,6 @@ async function connect() {
   const providerOptions = {
     // torus: {
     //   package: Torus,
-    // },
-    // walletconnect: {
-    //   package: window.WalletConnectProvider.default,
-    //   options: {
-    //     infuraId: "00000000000000000000000000000000",
-    //   },
     // },
   };
 
