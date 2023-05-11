f39c99a5289c3e0c2d4864be8a6dd864b33f7e74
creeppak <oleksandr@chainsafe.io>
2023-05-11 15:36:32 +0300

    Removed _checkProvider from JsonRpcSigner

--- src/ChainSafe.GamingSDK.EVM/Web3/Core/Evm/BaseSigner.cs
+++ src/ChainSafe.GamingSDK.EVM/Web3/Core/Evm/BaseSigner.cs
@@ -13,13 +13,13 @@ namespace Web3Unity.Scripts.Library.Ethers.Signers
     {
         public IEvmProvider Provider { get; }
 
+        public abstract bool Connected { get; }
+        public abstract ValueTask Connect();
+
         protected BaseSigner(IEvmProvider provider)
         {
             Provider = provider;
         }
-
-        public abstract bool Connected { get; }
-        public abstract ValueTask Connect();
 
         // TODO: specific reason why these functions weren't abstract?
         public abstract Task<string> GetAddress();
@@ -36,25 +36,21 @@ namespace Web3Unity.Scripts.Library.Ethers.Signers
 
         public virtual async Task<HexBigInteger> GetBalance(BlockParameter blockTag = null)
         {
-            _checkProvider("GetBalance");
             return await Provider.GetBalance(await GetAddress(), blockTag);
         }
 
         public virtual async Task<HexBigInteger> GetTransactionCount(BlockParameter blockTag = null)
         {
-            _checkProvider("GetTransactionCount");
             return await Provider.GetTransactionCount(await GetAddress(), blockTag);
         }
 
         public virtual async Task<HexBigInteger> EstimateGas(TransactionRequest transaction)
         {
-            _checkProvider("EstimateGas");
             return await Provider.EstimateGas(transaction);
         }
 
         public virtual async Task<string> Call(TransactionRequest transaction, BlockParameter blockTag = null)
         {
-            _checkProvider("Call");
             return await Provider.Call(transaction, blockTag);
         }
 
@@ -62,39 +58,17 @@ namespace Web3Unity.Scripts.Library.Ethers.Signers
 
         public virtual async Task<ulong> GetChainId()
         {
-            _checkProvider("GetChainId");
             return (await Provider.GetNetwork()).ChainId;
         }
 
         public virtual async Task<HexBigInteger> GetGasPrice()
         {
-            _checkProvider("GetGasPrice");
             return await Provider.GetGasPrice();
         }
 
         public virtual async Task<FeeData> GetFeeData()
         {
-            _checkProvider("GetFeeData");
             return await Provider.GetFeeData();
-        }
-
-        private void _checkProvider(string operation)
-        {
-            if (Provider == null)
-            {
-                _captureError(operation, "missing provider");
-                throw new Exception("missing provider");
-            }
-        }
-
-        private static void _captureError(string operation, string error)
-        {
-            var properties = new Dictionary<string, object>
-            {
-                {"error", error}
-            };
-
-            //DataDog.Client.Capture("Chains", properties);
         }
     }
 }
