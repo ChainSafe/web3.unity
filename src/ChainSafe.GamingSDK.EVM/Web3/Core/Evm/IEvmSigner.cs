e0bb7ce2fafbfe8e77571538afc15b90e308dfe3
Arshia001 <Arshia001@users.noreply.github.com>
2023-06-08 15:28:26 +0400

    Revert "Update IEvmSigner (#450)" (#457)
    This reverts commit 82bf35f949423733f9f6cb38a86cb209798c27f4.

--- src/ChainSafe.GamingSDK.EVM/Web3/Core/Evm/IEvmSigner.cs
+++ src/ChainSafe.GamingSDK.EVM/Web3/Core/Evm/IEvmSigner.cs
@@ -15,8 +15,6 @@ namespace Web3Unity.Scripts.Library.Ethers.Signers
 
         public Task<string> GetAddress();
 
-        public string GetAddress(string pk);
-
         public Task<string> SignMessage(byte[] message);
 
         public Task<string> SignMessage(string message);
