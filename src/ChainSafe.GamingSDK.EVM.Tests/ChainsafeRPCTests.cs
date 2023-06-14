48ffa2a39373497c85c0543d65fb9d72deeeb2a6
Arshia001 <Arshia001@users.noreply.github.com>
2023-06-14 18:27:47 +0400

    Move remaining commits over from EthereumRPCSharp repo (#470)

--- src/ChainSafe.GamingSDK.EVM.Tests/ChainsafeRPCTests.cs
+++ src/ChainSafe.GamingSDK.EVM.Tests/ChainsafeRPCTests.cs
@@ -1,50 +1,78 @@
-﻿using System.Numerics;
+﻿using System.Threading.Tasks;
+using ChainSafe.GamingSDK.EVM.Web3.Core;
+using ChainSafe.GamingSDK.EVM.Web3.Core.Evm;
+using ChainSafe.GamingWeb3.Build;
+using Microsoft.Extensions.DependencyInjection;
 using Nethereum.Hex.HexTypes;
 using Nethereum.RPC.Eth.DTOs;
 using NUnit.Framework;
 using Web3Unity.Scripts.Library.Ethers.Contracts;
+using Web3Unity.Scripts.Library.Ethers.JsonRpc;
+using Web3Unity.Scripts.Library.Ethers.NetCore;
 using Web3Unity.Scripts.Library.Ethers.Providers;
+using Web3Unity.Scripts.Library.Ethers.Signers;
 using Web3Unity.Scripts.Library.Ethers.Transactions;
 using Web3Unity.Scripts.Library.Ethers.Utils;
 
 namespace ChainSafe.GamingSDK.EVM.Tests
 {
+    using Web3 = ChainSafe.GamingWeb3.Web3;
+
     [TestFixture]
     public class ChainSafeRpcTests
     {
-        private JsonRpcProvider infuraProvider;
-        private JsonRpcProvider ganacheProvider;
+        // todo: we need to actually seed the Ganache instance with some transactions before the tests will run
+        private Web3 web3;
         private string goerliAddress;
-        /*
+        private string contractAddress;
         private string nftAddress;
-        private string nftABi;
-        */
+        private string nftAbi;
 
         [OneTimeSetUp]
-        public void SetUp()
+        public async Task SetUp()
         {
-            ganacheProvider = ProviderMigration.NewJsonRpcProviderAsync("http://127.0.0.1:7545").Result;
-            infuraProvider = ProviderMigration.NewJsonRpcProviderAsync("https://goerli.infura.io/v3/904006115c764661965dc0909e5ed473").Result;
+            web3 = await new GamingWeb3.Build.Web3Builder(
+                new ProjectConfig { ProjectId = string.Empty },
+                new ChainConfig
+                {
+                    Chain = "Ganache",
+                    ChainId = "88888888",
+                    Network = "Geth Testnet",
+                    Rpc = "http://127.0.0.1:7545",
+                })
+                .Configure(services =>
+                {
+                    services.UseNetCoreEnvironment();
+                    services.UseJsonRpcProvider();
+
+                    services.AddSingleton<JsonRpcWalletConfiguration>();
+                    services.AddSingleton<ISigner, ITransactionExecutor, ILifecycleParticipant, JsonRpcWallet>();
+                })
+                .BuildAsync();
+
             goerliAddress = "0xaBed4239E4855E120fDA34aDBEABDd2911626BA1";
-            /*
             nftAddress = "0xc81fa2eacc1c45688d481b11ce94c24a136e125d";
-            nftABi =
+            nftAbi =
                 "[{\"inputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"constructor\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":true,\"internalType\":\"address\",\"name\":\"owner\",\"type\":\"address\"},{\"indexed\":true,\"internalType\":\"address\",\"name\":\"approved\",\"type\":\"address\"},{\"indexed\":true,\"internalType\":\"uint256\",\"name\":\"tokenId\",\"type\":\"uint256\"}],\"name\":\"Approval\",\"type\":\"event\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":true,\"internalType\":\"address\",\"name\":\"owner\",\"type\":\"address\"},{\"indexed\":true,\"internalType\":\"address\",\"name\":\"operator\",\"type\":\"address\"},{\"indexed\":false,\"internalType\":\"bool\",\"name\":\"approved\",\"type\":\"bool\"}],\"name\":\"ApprovalForAll\",\"type\":\"event\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":true,\"internalType\":\"address\",\"name\":\"from\",\"type\":\"address\"},{\"indexed\":true,\"internalType\":\"address\",\"name\":\"to\",\"type\":\"address\"},{\"indexed\":true,\"internalType\":\"uint256\",\"name\":\"tokenId\",\"type\":\"uint256\"}],\"name\":\"Transfer\",\"type\":\"event\"},{\"inputs\":[{\"internalType\":\"address\",\"name\":\"to\",\"type\":\"address\"},{\"internalType\":\"uint256\",\"name\":\"tokenId\",\"type\":\"uint256\"}],\"name\":\"approve\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"address\",\"name\":\"owner\",\"type\":\"address\"}],\"name\":\"balanceOf\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"uint256\",\"name\":\"tokenId\",\"type\":\"uint256\"}],\"name\":\"getApproved\",\"outputs\":[{\"internalType\":\"address\",\"name\":\"\",\"type\":\"address\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"address\",\"name\":\"owner\",\"type\":\"address\"},{\"internalType\":\"address\",\"name\":\"operator\",\"type\":\"address\"}],\"name\":\"isApprovedForAll\",\"outputs\":[{\"internalType\":\"bool\",\"name\":\"\",\"type\":\"bool\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"mint\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"name\",\"outputs\":[{\"internalType\":\"string\",\"name\":\"\",\"type\":\"string\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"uint256\",\"name\":\"tokenId\",\"type\":\"uint256\"}],\"name\":\"ownerOf\",\"outputs\":[{\"internalType\":\"address\",\"name\":\"\",\"type\":\"address\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"address\",\"name\":\"from\",\"type\":\"address\"},{\"internalType\":\"address\",\"name\":\"to\",\"type\":\"address\"},{\"internalType\":\"uint256\",\"name\":\"tokenId\",\"type\":\"uint256\"}],\"name\":\"safeTransferFrom\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"address\",\"name\":\"from\",\"type\":\"address\"},{\"internalType\":\"address\",\"name\":\"to\",\"type\":\"address\"},{\"internalType\":\"uint256\",\"name\":\"tokenId\",\"type\":\"uint256\"},{\"internalType\":\"bytes\",\"name\":\"_data\",\"type\":\"bytes\"}],\"name\":\"safeTransferFrom\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"address\",\"name\":\"operator\",\"type\":\"address\"},{\"internalType\":\"bool\",\"name\":\"approved\",\"type\":\"bool\"}],\"name\":\"setApprovalForAll\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"bytes4\",\"name\":\"interfaceId\",\"type\":\"bytes4\"}],\"name\":\"supportsInterface\",\"outputs\":[{\"internalType\":\"bool\",\"name\":\"\",\"type\":\"bool\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"symbol\",\"outputs\":[{\"internalType\":\"string\",\"name\":\"\",\"type\":\"string\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"uint256\",\"name\":\"tokenId\",\"type\":\"uint256\"}],\"name\":\"tokenURI\",\"outputs\":[{\"internalType\":\"string\",\"name\":\"\",\"type\":\"string\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"address\",\"name\":\"from\",\"type\":\"address\"},{\"internalType\":\"address\",\"name\":\"to\",\"type\":\"address\"},{\"internalType\":\"uint256\",\"name\":\"tokenId\",\"type\":\"uint256\"}],\"name\":\"transferFrom\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"}]";
-            */
+
+            // Deploy a contract with https://remix.ethereum.org/ and your Ganache instance.
+            // Make sure to select Ganache Provider as the Environment. Once deployed, replace the _contractAddress value with your contract address
+            // todo: This is impossible to automate
+            contractAddress = "0x2061c2B28F007DD0eD5064A61d352521CC1d2827";
         }
 
         [Test]
         public void GetNetworkTest()
         {
-            var network = infuraProvider.GetNetwork().Result;
-            Assert.AreEqual("Goerli", network.Name);
-            Assert.AreEqual(5, network.ChainId);
+            var network = web3.RpcProvider.GetNetwork().Result;
+            Assert.AreEqual("Geth Testnet", network.Name);
+            Assert.AreEqual(1337, network.ChainId);
         }
 
         [Test]
         public void GetBalanceTest()
         {
-            var balance = infuraProvider.GetBalance(goerliAddress).Result;
+            var balance = web3.RpcProvider.GetBalance(goerliAddress).Result;
             var balanceFormatted = Units.FormatEther(balance);
             Assert.Greater(balanceFormatted, "0");
         }
@@ -52,16 +80,16 @@ namespace ChainSafe.GamingSDK.EVM.Tests
         [Test]
         public void GetCodeTest()
         {
-            var code = infuraProvider.GetCode(goerliAddress).Result;
+            var code = web3.RpcProvider.GetCode(goerliAddress).Result;
             Assert.AreEqual("0x", code);
         }
 
         [Test]
         public void GetLastBlockTest()
         {
-            var latestBlock = infuraProvider.GetBlock().Result;
+            var latestBlock = web3.RpcProvider.GetBlock().Result;
             Assert.AreEqual("0x0000000000000000", latestBlock.Nonce);
-            Assert.AreEqual("30000000", latestBlock.GasLimit.ToString());
+            Assert.AreEqual("6721975", latestBlock.GasLimit.ToString());
             Assert.True(latestBlock.BlockHash.StartsWith("0x"));
             Assert.True(latestBlock.ParentHash.StartsWith("0x"));
             Assert.IsNotEmpty(latestBlock.Number.ToString());
@@ -71,24 +99,24 @@ namespace ChainSafe.GamingSDK.EVM.Tests
         [Test]
         public void GetBlockNumberTest()
         {
-            var currentBlockNumber = infuraProvider.GetBlockNumber().Result;
+            var currentBlockNumber = web3.RpcProvider.GetBlockNumber().Result;
             Assert.Greater(currentBlockNumber.ToString(), "0");
         }
 
         [Test]
         public void GetBlockByNumberTest()
         {
-            var currentBlockNumber = infuraProvider.GetBlockNumber().Result;
-            var blockByNumber = infuraProvider.GetBlock(new BlockParameter(currentBlockNumber)).Result;
+            var currentBlockNumber = web3.RpcProvider.GetBlockNumber().Result;
+            var blockByNumber = web3.RpcProvider.GetBlock(new BlockParameter(currentBlockNumber)).Result;
             Assert.AreEqual(blockByNumber.Number, currentBlockNumber);
         }
 
         [Test]
         public void GetBlockWithTransactionsTest()
         {
-            var currentBlockNumber = infuraProvider.GetBlockNumber().Result;
+            var currentBlockNumber = web3.RpcProvider.GetBlockNumber().Result;
             var blockParameter = new BlockParameter(currentBlockNumber.ToUlong());
-            var blockWithTx = infuraProvider.GetBlockWithTransactions(blockParameter).Result;
+            var blockWithTx = web3.RpcProvider.GetBlockWithTransactions(blockParameter).Result;
 
             var firstTransaction = blockWithTx.Transactions[0];
             Assert.AreEqual(5, firstTransaction.ChainId.ToUlong());
@@ -98,9 +126,9 @@ namespace ChainSafe.GamingSDK.EVM.Tests
         [Test]
         public void GetPreviousBlockTest()
         {
-            var currentBlockNumber = infuraProvider.GetBlockNumber().Result;
+            var currentBlockNumber = web3.RpcProvider.GetBlockNumber().Result;
             var previousBlockNumber = currentBlockNumber.ToUlong() - 1;
-            var previousBlock = infuraProvider.GetBlock(new BlockParameter(previousBlockNumber)).Result;
+            var previousBlock = web3.RpcProvider.GetBlock(new BlockParameter(previousBlockNumber)).Result;
             Assert.AreEqual(previousBlockNumber, previousBlock.Number.ToUlong());
             Assert.True(previousBlock.BlockHash.StartsWith("0x"));
         }
@@ -108,14 +136,14 @@ namespace ChainSafe.GamingSDK.EVM.Tests
         [Test]
         public void GetTransactionCountTest()
         {
-            var txCount = infuraProvider.GetTransactionCount(goerliAddress).Result;
+            var txCount = web3.RpcProvider.GetTransactionCount(goerliAddress).Result;
             Assert.Greater(txCount.ToUlong(), 100);
         }
 
         [Test]
         public void GetFeeDataTest()
         {
-            var feeData = infuraProvider.GetFeeData().Result;
+            var feeData = web3.RpcProvider.GetFeeData().Result;
             Assert.Greater(Units.FormatUnits(feeData.MaxFeePerGas, "gwei"), "0");
             Assert.Greater(Units.FormatUnits(feeData.GasPrice, "gwei"), "0");
             Assert.AreEqual(Units.FormatUnits(feeData.MaxPriorityFeePerGas, "gwei"), "1.5");
@@ -124,7 +152,7 @@ namespace ChainSafe.GamingSDK.EVM.Tests
         [Test]
         public void GetGasPriceTest()
         {
-            var gasPrice = infuraProvider.GetGasPrice().Result;
+            var gasPrice = web3.RpcProvider.GetGasPrice().Result;
             var gwei = Units.FormatUnits(gasPrice, "gwei");
             Assert.Greater(gasPrice.ToString(), "0");
             Assert.Greater(gasPrice.ToString(), gwei);
@@ -133,104 +161,94 @@ namespace ChainSafe.GamingSDK.EVM.Tests
         [Test]
         public void GetTransactionReceiptTest()
         {
-            var latestBlockWithTx = infuraProvider.GetBlockWithTransactions().Result;
+            var latestBlockWithTx = web3.RpcProvider.GetBlockWithTransactions().Result;
             var firstTransactionInBlock = latestBlockWithTx.Transactions[0];
             var receipt =
-                infuraProvider.GetTransactionReceipt(
+                web3.RpcProvider.GetTransactionReceipt(
                     firstTransactionInBlock.Hash).Result;
             Assert.AreEqual(firstTransactionInBlock.Hash, receipt.TransactionHash);
             Assert.AreEqual(firstTransactionInBlock.BlockHash, receipt.BlockHash);
         }
 
         /*
+         * To run this test you need to deploy a contract using your Ganache instance and the contract needs to have a safeMint function that increments the tokenId counter
+         * Example contract .sol file:
+         * contract Contract is ERC721, Ownable {
+                using Counters for Counters.Counter;
+                Counters.Counter private _tokenIdCounter;
+                constructor() ERC721("Contract", "MNFT") {}
+
+                function safeMint(address to) public onlyOwner {
+                    uint256 tokenId = _tokenIdCounter.current();
+                    _tokenIdCounter.increment();
+                    _safeMint(to, tokenId);
+                }
+            }
+         */
         [Test]
         public void CallContractMethodTest()
         {
-            var contract = new Contract(nftABi, nftAddress, infuraProvider);
+            var address = web3.Signer.GetAddress().Result;
+            var contract = web3.ContractBuilder.Build(nftAbi, contractAddress);
+
+            var ret = contract.Send("safeMint", new object[] { address }).Result;
 
             var name = contract.Call("name").Result;
-            Assert.AreEqual("MyNFT", name[0]);
+            Assert.AreEqual("Contract", name[0]);
 
             var symbol = contract.Call("symbol").Result;
             Assert.AreEqual("MNFT", symbol[0]);
 
-            var tokenUri = contract.Call("tokenURI", new object[] { 1 }).Result;
-            Assert.AreEqual("http://mynft.com/1", tokenUri[0]);
+            var tokenUri = contract.Call("tokenURI", new object[] { 0 }).Result;
+            Assert.AreEqual(string.Empty, tokenUri[0]);
 
-            var ownerOf = contract.Call("ownerOf", new object[] { 1 }).Result;
-            Assert.AreEqual("0x7f686f9B2740e09469a4A5A0F2eDE2d317D4aeE5", ownerOf[0]);
+            var ownerOf = contract.Call("ownerOf", new object[] { 0 }).Result;
+            StringAssert.AreEqualIgnoringCase(address, ownerOf[0].ToString());
 
             var balanceOf = contract.Call("balanceOf", new[] { ownerOf[0] }).Result;
-            Assert.AreEqual(new BigInteger(6), balanceOf[0]);
+            Assert.GreaterOrEqual(balanceOf[0].ToString(), "1");
         }
-        */
 
-        /*
         [Test]
         public void EstimateGasContractMethodTest()
         {
-            var contract = new Contract(nftABi, goerliAddress, infuraProvider);
+            var contract = web3.ContractBuilder.Build(nftAbi, goerliAddress);
             var result = contract.EstimateGas("ownerOf", new object[] { 1 }).Result;
             Assert.AreEqual("21204", result.ToString());
         }
-        */
 
         [Test]
         public void GetAccountsTest()
         {
-            var accounts = ganacheProvider.ListAccounts().Result;
+            var accounts = (web3.RpcProvider as JsonRpcProvider).ListAccounts().Result;
             Assert.AreEqual(10, accounts.Length);
             foreach (var account in accounts)
             {
-                var accountBalance = ganacheProvider.GetBalance(account).Result;
+                var accountBalance = web3.RpcProvider.GetBalance(account).Result;
                 Assert.GreaterOrEqual(Units.FormatEther(accountBalance), "0");
             }
         }
 
-        // todo: are these tests needed to test something other than the
-        // now removed JsonRpcSigner?
-        // [Test]
-        // public void GetSignerTest()
-        // {
-        //     TestHelper.VerifyGanacheConnection(ganacheProvider);
-        //
-        //     var signer = ganacheProvider.GetSigner();
-        //     var accounts = ganacheProvider.ListAccounts().Result;
-        //     Assert.AreEqual(accounts[0], signer.GetAddress().Result);
-        //
-        //     var accountBalance = ganacheProvider.GetBalance(accounts[0]).Result;
-        //     Assert.AreEqual(accountBalance, signer.GetBalance().Result);
-        //     Assert.AreEqual(1337, signer.GetChainId().Result);
-        // }
-        //
-        // [Test]
-        // public void SendContractTest()
-        // {
-        //     TestHelper.VerifyGanacheConnection(ganacheProvider);
-        //
-        //     var signer = ganacheProvider.GetSigner();
-        //
-        //     var contract = new Contract(nftABi, nftAddress, ganacheProvider);
-        //     var ret = contract.Connect(signer).Send("mint").Result;
-        //
-        //     Assert.IsNotNull(ret);
-        // }
-        //
-        // [Test]
-        // public void SendContractOverrideGasTest()
-        // {
-        //     TestHelper.VerifyGanacheConnection(ganacheProvider);
-        //
-        //     var signer = ganacheProvider.GetSigner();
-        //
-        //     var contract = new Contract(nftABi, nftAddress, ganacheProvider);
-        //     var ret = contract.Attach(nftAddress).Connect(signer).Send("mint", null, new TransactionRequest
-        //     {
-        //         GasLimit = new HexBigInteger("10000"),
-        //         GasPrice = new HexBigInteger("100000000"),
-        //     }).Result;
-        //
-        //     Assert.IsNotNull(ret);
-        // }
+        [Test]
+        public void SendContractTest()
+        {
+            var contract = web3.ContractBuilder.Build(nftAbi, nftAddress);
+            var ret = contract.Send("safeMint").Result;
+
+            Assert.IsNotNull(ret);
+        }
+
+        [Test]
+        public void SendContractOverrideGasTest()
+        {
+            var contract = web3.ContractBuilder.Build(nftAbi, nftAddress);
+            var ret = contract.Send("mint", null, new TransactionRequest
+            {
+                GasLimit = new HexBigInteger("10000"),
+                GasPrice = new HexBigInteger("100000000"),
+            }).Result;
+
+            Assert.IsNotNull(ret);
+        }
     }
 }
