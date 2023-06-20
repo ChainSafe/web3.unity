8875fc3cd7d5b051dad6ee847b0c8ef1cd64db00
Arshia001 <Arshia001@users.noreply.github.com>
2023-06-20 17:04:49 +0400

    Run a non-unity restore after building the Unity project (#476)

--- src/ChainSafe.GamingSdk.EVM.Unity/publish-to-unity-package.bat
+++ src/ChainSafe.GamingSdk.EVM.Unity/publish-to-unity-package.bat
@@ -1,19 +1,26 @@
 @ECHO OFF
 
-echo Building project . . .
+echo Building project...
 
 del obj /F /Q
 del bin /F /Q
 dotnet publish -c release -f netstandard2.1 /property:Unity=true
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-echo Moving files to Unity package . . .
+echo Restoring non-Unity packages...
 
-cd bin\release\netstandard2.1\publish
+pushd ..\..
+dotnet restore
+popd
+
+echo Moving files to Unity package...
+
+pushd bin\release\netstandard2.1\publish
 del Newtonsoft.Json.dll
 del UnityEngine.dll
 if not exist ..\..\..\..\..\UnityPackages\io.chainsafe.web3-unity\Runtime\Libraries mkdir ..\..\..\..\..\UnityPackages\io.chainsafe.web3-unity\Runtime\Libraries\
 del ..\..\..\..\..\UnityPackages\io.chainsafe.web3-unity\Runtime\Libraries\* /F /Q
 copy *.dll ..\..\..\..\..\UnityPackages\io.chainsafe.web3-unity\Runtime\Libraries
+popd
 
 echo Done
