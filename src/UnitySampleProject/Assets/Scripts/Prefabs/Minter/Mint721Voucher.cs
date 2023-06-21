eff32925f1c59f55ff7fdd9a5fbe7b7b9c51082c
Arshia Ghafoori <arshia001@live.com>
2023-06-21 13:20:51 +0400

    Lint

--- src/UnitySampleProject/Assets/Scripts/Prefabs/Minter/Mint721Voucher.cs
+++ src/UnitySampleProject/Assets/Scripts/Prefabs/Minter/Mint721Voucher.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 using Web3Unity.Scripts.Library.ETHEREUEM.Connect;
 using Web3Unity.Scripts.Library.Ethers.Transactions;
 
-public class Mint721Voucher: MonoBehaviour
+public class Mint721Voucher : MonoBehaviour
 {
     // address of nft you want to mint
     public string nftAddress = "f01559ae4021a47e26bc773587278f62a833f2a6117411afbc5a7855661936d1c";
