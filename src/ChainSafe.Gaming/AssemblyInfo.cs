377487d4e6490190280bca1eb4b9f5456948ad1e
creeppak <oleksandr@chainsafe.io>
2023-06-22 17:59:00 +0300

    Renamed ChainSafe.GamingSdk.EVM.NetCore project to ChainSafe.Gaming.NetCore

--- src/ChainSafe.Gaming/AssemblyInfo.cs
+++ src/ChainSafe.Gaming/AssemblyInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("ChainSafe.GamingSdk.EVM.NetCore")]
+[assembly: InternalsVisibleTo("ChainSafe.Gaming.NetCore")]
 [assembly: InternalsVisibleTo("ChainSafe.GamingSdk.EVM.Unity")]
