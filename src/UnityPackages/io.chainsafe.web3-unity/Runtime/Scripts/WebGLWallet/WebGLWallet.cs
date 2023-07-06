320e2c80f181838e3a5858de175316f14f7bb262
Ryan Noble <ryanjnoble@gmail.com>
2023-07-06 20:11:44 +0200

    Added functions for WebGL wallet

--- src/UnityPackages/io.chainsafe.web3-unity/Runtime/Scripts/WebGLWallet/WebGLWallet.cs
+++ src/UnityPackages/io.chainsafe.web3-unity/Runtime/Scripts/WebGLWallet/WebGLWallet.cs
@@ -103,7 +103,18 @@ namespace ChainSafe.GamingSDK.EVM.WebGLWallet
         // todo: implement before release
         public Task<string> SignTypedData<TStructType>(SerializableDomain domain, Dictionary<string, MemberDescription[]> types, TStructType message)
         {
-            throw new NotImplementedException();
+            JS_resetSignTypedMessageResponse();
+            JS_signTypedMessage(domain, types, message);
+            var signedTypedMessageResponse = await PollJsSide(JS_getSignTypedMessageResponse);
+            AssertResponseSuccessful(signedTypedMessageResponse);
+            return signedTypedMessageResponse;
+
+            void AssertResponseSuccessful(string response)
+            {
+                // todo: check with regex mb?
+                if (response.Length == 132) return;
+                throw new Web3Exception("Sign message operation was rejected.");
+            }
         }
 
         // todo: will break if running two of the same Poll tasks from different signers
@@ -133,6 +144,14 @@ namespace ChainSafe.GamingSDK.EVM.WebGLWallet
         private static extern string JS_getSignMessageResponse();
         [DllImport("__Internal")]
         private static extern void JS_resetSignMessageResponse();
+        
+        // SignMessage
+        [DllImport("__Internal")]
+        private static extern void JS_signTypedMessage<TStructType>(SerializableDomain domain, Dictionary<string, MemberDescription[]> types, TStructType message);
+        [DllImport("__Internal")]
+        private static extern string JS_getSignTypedMessageResponse();
+        [DllImport("__Internal")]
+        private static extern void JS_resetSignTypedMessageResponse();
 
         // SendTransaction (no data)
         [DllImport("__Internal")]
