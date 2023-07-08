e48c67c543f6a99099283f14c6ac05d1a1fa5482
Arshia Ghafoori <arshia001@live.com>
2023-07-08 18:49:09 +0400

    Fix #492

--- src/UnitySampleProject/Assets/Scripts/Samples/ERC20/ERC20CustomTokenBalance.cs
+++ src/UnitySampleProject/Assets/Scripts/Samples/ERC20/ERC20CustomTokenBalance.cs
@@ -4,12 +4,15 @@ using Web3Unity.Scripts.Library.Ethers.Contracts;
 
 public class ERC20CustomTokenBalance : MonoBehaviour
 {
+    // abi in json format
+    [SerializeField]
+    string contractAbi;
+    // address of contract
+    [SerializeField]
+    string contractAddress;
+
     async void Start()
     {
-        // abi in json format
-        string contractAbi = "YOUR_TOKEN_ABI";
-        // address of contract
-        string contractAddress = "YOUR_TOKEN_ADDRESS";
         var contract = Web3Accessor.Web3.ContractBuilder.Build(contractAbi, contractAddress);
         var calldata = await contract.Call("balanceOf", new object[]
         {
