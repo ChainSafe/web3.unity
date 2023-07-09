7799ba802459ff232b55dcb640f79b1e9b4e184c
kwame <kwame.bryan@gmail.com>
2023-07-08 23:02:38 -0400

    Update EthereumService.cs
    Added xmldoc to Ethereum Service

--- src/UnitySampleProject/Assets/Scripts/Prefabs/Web3AuthWallet/Services/EthereumService.cs
+++ src/UnitySampleProject/Assets/Scripts/Prefabs/Web3AuthWallet/Services/EthereumService.cs
@@ -9,29 +9,51 @@ using UnityEngine;
 
 namespace Prefabs.Web3AuthWallet.Utils
 {
+    /// <summary>
+    /// Provides Ethereum-related functionality.
+    /// </summary>
     public class EthereumService : IEthereumService
     {
         private readonly Web3 _web3;
         private Account _account;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EthereumService"/> class.
+        /// </summary>
+        /// <param name="privateKey">The private key associated with the Ethereum account.</param>
+        /// <param name="url">The URL of the Ethereum node.</param>
         public EthereumService(string privateKey, string url)
         {
             _web3 = new Web3(new Account(privateKey), url);
         }
 
+        /// <summary>
+        /// Creates and signs a transaction asynchronously.
+        /// </summary>
+        /// <param name="txInput">The transaction input parameters.</param>
+        /// <returns>The signed transaction data as a string.</returns>
         public async Task<string> CreateAndSignTransactionAsync(TransactionInput txInput)
         {
-            Debug.Log("Transaction Data: " + txInput.Data);
             var signedTransaction = await _web3.Eth.TransactionManager.SignTransactionAsync(txInput);
 
             return signedTransaction;
         }
 
+        /// <summary>
+        /// Sends a signed transaction asynchronously.
+        /// </summary>
+        /// <param name="signedTransactionData">The signed transaction data.</param>
+        /// <returns>The transaction hash as a string.</returns>
         public async Task<string> SendTransactionAsync(string signedTransactionData)
         {
             return await _web3.Eth.Transactions.SendRawTransaction.SendRequestAsync(signedTransactionData);
         }
 
+        /// <summary>
+        /// Gets the Ethereum address associated with the provided private key.
+        /// </summary>
+        /// <param name="privateKey">The private key.</param>
+        /// <returns>The Ethereum address as a string.</returns>
         public string GetAddressW3A(string privateKey) => new EthECKey(privateKey).GetPublicAddress();
     }
 }
