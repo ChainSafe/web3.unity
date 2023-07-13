859f491a7589b21246425d4d74b12247f4c10603
Ryan Noble <ryanjnoble@gmail.com>
2023-07-13 16:18:04 +0200

    Added general layout & config

---
+++ src/ChainSafe.GamingSDK.EVM/Web3/Core/Evm/MultiCall/IMultiCall.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Web3Unity.Scripts.Library.Ethers.Providers;
+
+namespace ChainSafe.GamingSDK.EVM.Web3.Core.Evm.MultiCall
+{
+    public interface IMultiCall : ILifecycleParticipant
+    {
+        void MultiCall(IRpcProvider provider);
+    }
+}
---
+++ src/ChainSafe.GamingSDK.EVM/Web3/Core/Evm/MultiCall/MultiCall.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Web3Unity.Scripts.Library.Ethers.Providers;
+using Web3Unity.Scripts.Library.Ethers.Signers;
+
+namespace ChainSafe.GamingSDK.EVM.Web3.Core.Evm.MultiCall
+{
+    public class MultiCall : ILifecycleParticipant
+    {
+        private readonly IRpcProvider provider;
+
+        public MultiCall(IRpcProvider provider)
+        {
+            this.provider = provider;
+        }
+
+        public ValueTask WillStartAsync()
+        {
+        }
+
+        public ValueTask WillStopAsync()
+        {
+        }
+    }
+}
