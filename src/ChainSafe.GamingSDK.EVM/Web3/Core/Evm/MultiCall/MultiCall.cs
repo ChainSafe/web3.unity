38bfaae2c0d14094b3eeacdf850f255319c2aaf2
Ryan Noble <ryanjnoble@gmail.com>
2023-07-14 16:42:19 +0200

    Wiring up config

--- src/ChainSafe.GamingSDK.EVM/Web3/Core/Evm/MultiCall/MultiCall.cs
+++ src/ChainSafe.GamingSDK.EVM/Web3/Core/Evm/MultiCall/MultiCall.cs
@@ -1,24 +1,38 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using ChainSafe.GamingWeb3;
+using Web3Unity.Scripts.Library.Ethers.Contracts;
 using Web3Unity.Scripts.Library.Ethers.Providers;
-using Web3Unity.Scripts.Library.Ethers.Signers;
 
 namespace ChainSafe.GamingSDK.EVM.Web3.Core.Evm.MultiCall
 {
     public class MultiCall : ILifecycleParticipant
     {
-        private readonly IRpcProvider provider;
+        private Contract multiCallContract;
 
-        public MultiCall(IRpcProvider provider)
+        public MultiCall(IRpcProvider provider, IChainConfig chainConfig, MultiCallConfig config)
         {
-            this.provider = provider;
+            if (MultiCallConfig.DeployedNetworks.Contains(chainConfig.ChainId))
+            {
+                multiCallContract = new Contract(MultiCallConfig.MultiCallAbi, MultiCallConfig.OfficialAddress, provider);
+            }
+            else
+            {
+                if (config.CustomNetworks.TryGetValue(chainConfig.ChainId, out var address))
+                {
+                    multiCallContract = new Contract(MultiCallConfig.MultiCallAbi, address, provider);
+                }
+            }
         }
 
         public ValueTask WillStartAsync()
         {
+            return default;
         }
 
         public ValueTask WillStopAsync()
         {
+            return default;
         }
     }
 }
