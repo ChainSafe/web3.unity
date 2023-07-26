dcfdee4dd32f6ae226ca22fd440390640c3d84e4
Ryan Noble <ryanjnoble@gmail.com>
2023-07-26 15:50:28 +0200

    Integrating into Unity

--- src/ChainSafe.GamingSDK.EVM/Web3/Core/Evm/MultiCall3/MultiCallConfig.cs
+++ src/ChainSafe.GamingSDK.EVM/Web3/Core/Evm/MultiCall3/MultiCallConfig.cs
@@ -1,17 +1,14 @@
-﻿#nullable enable
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace ChainSafe.GamingSDK.EVM.Web3.Core.Evm.MultiCall3
 {
-    [Serializable]
     public class MultiCallConfig
     {
-        public Dictionary<string, string> CustomNetworks;
-
         public MultiCallConfig(Dictionary<string, string> customNetworks)
         {
-            this.CustomNetworks = customNetworks;
+            CustomNetworks = customNetworks;
         }
+
+        public IReadOnlyDictionary<string, string> CustomNetworks { get; }
     }
 }
---
+++ src/UnitySampleProject/Assets/Scripts/Prefabs/MultiCall/MultiCall.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiCall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public async void FetchBatchBalances()
+    {
+        
+    }
+}
