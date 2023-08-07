c7f03fe6fa6e4083213a11e3a2bc8a1fb317fe33
Peter Kalambet <peter@kalambet.dev>
2023-08-07 20:15:25 +0200

    Add gas estimation to the Contract execution

--- src/UnitySampleProject/Assets/Scripts/Samples/Wallet/ContractSend.cs
+++ src/UnitySampleProject/Assets/Scripts/Samples/Wallet/ContractSend.cs
@@ -1,7 +1,7 @@
 using System;
 using UnityEngine;
 
-public class ContractCall : MonoBehaviour
+public class ContractSend : MonoBehaviour
 {
     async public void Start()
     {
@@ -9,7 +9,8 @@ public class ContractCall : MonoBehaviour
         string abi = "[ { \"inputs\": [ { \"internalType\": \"uint8\", \"name\": \"_myArg\", \"type\": \"uint8\" } ], \"name\": \"addTotal\", \"outputs\": [], \"stateMutability\": \"nonpayable\", \"type\": \"function\" }, { \"inputs\": [], \"name\": \"myTotal\", \"outputs\": [ { \"internalType\": \"uint256\", \"name\": \"\", \"type\": \"uint256\" } ], \"stateMutability\": \"view\", \"type\": \"function\" } ]";
         string contractAddress = "0x7286Cf0F6E80014ea75Dbc25F545A3be90F4904F";
         var contract = Web3Accessor.Web3.ContractBuilder.Build(abi, contractAddress);
-        var response = await contract.Send(method, new object[] { 1 });
-        Debug.Log(response);
+        await contract.Send(method, new object[] { 1 });
+        // Method `addTotal` returns nothing, see ABI.
+        Debug.Log("Script is Done!");
     }
 }
