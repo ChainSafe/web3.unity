40400f97ce0b3098fafcd20c12989604be0019bf
Peter Kalambet <peter@kalambet.dev>
2023-08-30 13:06:09 +0200

    hack anvil execution a little bit

--- src/ChainSafe.GamingSDK.EVM.Tests/Node/Emulator.cs
+++ src/ChainSafe.GamingSDK.EVM.Tests/Node/Emulator.cs
@@ -11,7 +11,7 @@ namespace ChainSafe.GamingSDK.EVM.Tests.Node
         public static Process CreateInstance(uint port = 8545)
         {
             var anvilProc =
-                new ProcessStartInfo("anvil", $"-m \"{Mnemonic}\" -p {port}")
+                new ProcessStartInfo("anvil", $"-m \"{Mnemonic}\" -p {port} --silent")
                 {
                     CreateNoWindow = true,
                     WindowStyle = ProcessWindowStyle.Hidden,
@@ -29,8 +29,8 @@ namespace ChainSafe.GamingSDK.EVM.Tests.Node
                 throw new Web3Exception("Anvil is not starting");
             }
 
-            // Wait 2 seconds since it's hard to figure out when anvil finish starting
-            Thread.Sleep(2000);
+            // Wait 3 seconds since it's hard to figure out when anvil finish starting
+            Thread.Sleep(3000);
 
             return anvil;
         }
