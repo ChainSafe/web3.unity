b75be11b8e2028d428d48b248887c81c0fa615fb
rob1997 <robelgetnetrg@gmail.com>
2023-09-05 13:42:41 +0300

    Importable Sample Project (#591)
    * Changes Made
    + Moved sample project to an importable sample from Web3.Unity package
    + Added Initialization fixes to ProjectConfigUtilities, it can now create resource directory if it doesn't exist
    + Implemented SceneIndexer which basically adds sample scenes to Build Settings
    
    * Removed Tests folder

---
+++ src/UnitySampleProject/Assets/Samples/web3.unity SDK/2.5.0-pre001/Web3.Unity Samples/Scripts/Scenes/SampleNftTexture/LoadScene.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Scenes.SampleNftTexture
+{
+    public class LoadScene : MonoBehaviour
+    {
+        public string SceneName = "SampleMain";
+
+        public void Load()
+        {
+            SceneManager.LoadScene(SceneName);
+        }
+    }
+}
---
+++ src/UnitySampleProject/Assets/Samples/web3.unity SDK/2.5.0-pre001/Web3.Unity Samples/Scripts/Web3Accessor.cs
@@ -0,0 +1,53 @@
+using ChainSafe.GamingWeb3;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Web3Accessor : MonoBehaviour
+{
+    static Web3Accessor instance;
+
+    Web3 web3;
+
+    static Web3Accessor Instance
+    {
+        get
+        {
+            if (!instance)
+            {
+#if UNITY_EDITOR
+                // Having to switch to the first scene while working is a pain.
+                // Instead, we refuse to create an instance if the editor is
+                // currently running in any other scene and load the first scene
+                // instead.
+                if (SceneManager.GetActiveScene().buildIndex != 0)
+                {
+                    SceneManager.LoadScene(0);
+                    // Throw exception to prevent rest of code from running
+                    throw new System.Exception("Refusing to create Web3 instance since current scene is not first scene");
+                }
+#endif
+                var go = new GameObject("Web3Accessor");
+                DontDestroyOnLoad(go);
+                instance = go.AddComponent<Web3Accessor>();
+            }
+
+            return instance;
+        }
+    }
+
+    public static Web3 Web3 => Instance.web3;
+
+    public static Web3 TryWeb3 => instance ? instance.web3 : null;
+
+    public static void Set(Web3 web3)
+    {
+        if (Instance.web3 != null)
+        {
+            throw new System.Exception("Web3 instance was already initialized");
+        }
+
+        Instance.web3 = web3;
+    }
+
+    public static void Clear() => Instance.web3 = null;
+}
