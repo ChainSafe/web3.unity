f4f7869f5380a2483ba96b69aaaa1103c9bca599
oleksandr <oleksandr@chainsafe.io>
2023-09-06 15:13:26 +0300

    New SendWithReceipt method returns response + receipt

--- src/ChainSafe.GamingSDK.EVM/RPC/Contracts/Contract.cs
+++ src/ChainSafe.GamingSDK.EVM/RPC/Contracts/Contract.cs
@@ -90,6 +90,21 @@ namespace Web3Unity.Scripts.Library.Ethers.Contracts
         /// <returns>The outputs of the method.</returns>
         public async Task<object[]> Send(string method, object[] parameters = null, TransactionRequest overwrite = null)
         {
+            return (await SendWithReceipt(method, parameters, overwrite)).response;
+        }
+
+        /// <summary>
+        /// Sends the transaction.
+        /// </summary>
+        /// <param name="method">The method.</param>
+        /// <param name="parameters">The parameters.</param>
+        /// <param name="overwrite">An existing TransactionRequest to use instead of making a new one.</param>
+        /// <returns>The outputs of the method and the transaction receipt.</returns>
+        public async Task<(object[] response, TransactionReceipt receipt)> SendWithReceipt(
+            string method,
+            object[] parameters = null,
+            TransactionRequest overwrite = null)
+        {
             if (string.IsNullOrEmpty(address))
             {
                 throw new Exception("contract address is not set");
@@ -116,7 +131,8 @@ namespace Web3Unity.Scripts.Library.Ethers.Contracts
             var receipt = await provider.WaitForTransactionReceipt(tx.Hash);
 
             var output = function.DecodeOutput(tx.Data);
-            return output.Select(x => x.Result).ToArray();
+            var outputValues = output.Select(x => x.Result).ToArray();
+            return (outputValues, receipt);
         }
 
         /// <summary>
