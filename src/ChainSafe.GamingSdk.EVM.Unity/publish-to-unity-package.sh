650968def21aa93a3a2f32efc8920fc4e2194ddc
robGG1997 <robel@chainsafe.io>
2023-09-09 00:22:00 +0300

    some directory changes to move dlls

--- src/ChainSafe.GamingSdk.EVM.Unity/publish-to-unity-package.sh
+++ src/ChainSafe.GamingSdk.EVM.Unity/publish-to-unity-package.sh
@@ -10,11 +10,10 @@ dotnet publish -c release -f netstandard2.1 /property:Unity=true
 
 echo Moving files to Unity package...
 
-cd bin/release/netstandard2.1/publish
-rm Newtonsoft.Json.dll
-rm UnityEngine.dll
-mkdir -p ../../../../../UnityPackages/io.chainsafe.web3-unity/Runtime/Libraries
-rm -f ../../../../../UnityPackages/io.chainsafe.web3-unity/Runtime/Libraries/*
-cp *.dll ../../../../../UnityPackages/io.chainsafe.web3-unity/Runtime/Libraries
+rm -f bin/release/netstandard2.1/publish/Newtonsoft.Json.dll
+rm -f bin/release/netstandard2.1/publish/UnityEngine.dll
+mkdir -p ../UnityPackages/io.chainsafe.web3-unity/Runtime/Libraries
+rm -f ../UnityPackages/io.chainsafe.web3-unity/Runtime/Libraries/*
+cp *.dll ../UnityPackages/io.chainsafe.web3-unity/Runtime/Libraries
 
 echo Done
