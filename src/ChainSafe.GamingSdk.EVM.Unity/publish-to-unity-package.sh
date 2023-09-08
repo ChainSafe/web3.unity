0f142e6f16f7cbf780ed9a6357fccb72ccd372a8
robGG1997 <robel@chainsafe.io>
2023-09-09 01:29:16 +0300

    dir check

--- src/ChainSafe.GamingSdk.EVM.Unity/publish-to-unity-package.sh
+++ src/ChainSafe.GamingSdk.EVM.Unity/publish-to-unity-package.sh
@@ -8,12 +8,6 @@ rm -rf obj
 rm -rf bin
 dotnet publish -c release -f netstandard2.1 /property:Unity=true
 
-echo Moving files to Unity package...
-
-rm -f bin/release/netstandard2.1/publish/Newtonsoft.Json.dll
-rm -f bin/release/netstandard2.1/publish/UnityEngine.dll
-mkdir -p ../UnityPackages/io.chainsafe.web3-unity/Runtime/Libraries
-rm -f ../UnityPackages/io.chainsafe.web3-unity/Runtime/Libraries/*
 ls
 cd bin
 ls
@@ -23,6 +17,13 @@ cd netstandard2.1
 ls
 cd publish
 ls
+
+echo Moving files to Unity package...
+
+rm -f bin/release/netstandard2.1/publish/Newtonsoft.Json.dll
+rm -f bin/release/netstandard2.1/publish/UnityEngine.dll
+mkdir -p ../UnityPackages/io.chainsafe.web3-unity/Runtime/Libraries
+rm -f ../UnityPackages/io.chainsafe.web3-unity/Runtime/Libraries/*
 cp "bin/release/netstandard2.1/publish/BouncyCastle.Crypto.dll" "../UnityPackages/io.chainsafe.web3-unity/Runtime/Libraries"
 
 echo Done
