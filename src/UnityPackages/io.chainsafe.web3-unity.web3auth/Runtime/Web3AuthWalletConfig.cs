eab7d200f05b21d6dfe10e6f0f5deae6636cddb9
Peter Kalambet <peter@kalambet.dev>
2023-09-12 22:24:10 +0200

    WebGL Web3Auth Login Nikola's fix (#608)
    * linter + Nikola fix
    
    * Fixing a build issue
    
    * Testing settings in W3A
    
    * Login functional after allowing authorize session to start
    
    * Pushing for handover
    
    * fix lint rules and whitespaces
    
    * fix liner running on ci
    
    * Update src/UnitySampleProject/Assets/Samples/web3.unity SDK/2.5.0-pre001/Web3.Unity Samples/Scripts/Prefabs/Logout.cs
    
    Co-authored-by: Ryan Noble <ryanjnoble@gmail.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Ryan Noble <ryanjnoble@gmail.com>
    
    * update web3auth version
    
    * update web3auth version
    
    * update linter
    
    * Update src/UnityPackages/io.chainsafe.web3-unity.web3auth/Runtime/Plugins/Web3AuthSDK/Web3Auth.cs
    
    Co-authored-by: Ryan Noble <ryanjnoble@gmail.com>
    
    * Update src/UnityPackages/io.chainsafe.web3-unity.web3auth/Runtime/Web3AuthWallet.cs
    
    Co-authored-by: Ryan Noble <ryanjnoble@gmail.com>
    
    * Update src/UnityPackages/io.chainsafe.web3-unity.web3auth/Runtime/Web3AuthWallet.cs
    
    Co-authored-by: Ryan Noble <ryanjnoble@gmail.com>
    
    * Update src/UnityPackages/io.chainsafe.web3-unity.web3auth/Runtime/Web3AuthWallet.cs
    
    Co-authored-by: Ryan Noble <ryanjnoble@gmail.com>
    
    ---------
    
    Co-authored-by: Nikola Garabandic <kantagara@gmail.com>
    Co-authored-by: Ryan Noble <ryanjnoble@gmail.com>

--- src/UnityPackages/io.chainsafe.web3-unity.web3auth/Runtime/Web3AuthWalletConfig.cs
+++ src/UnityPackages/io.chainsafe.web3-unity.web3auth/Runtime/Web3AuthWalletConfig.cs
@@ -4,12 +4,6 @@ namespace ChainSafe.GamingSdk.Web3Auth
 {
     public class Web3AuthWalletConfig
     {
-        public string ClientId { get; set; }
-
-        public string RedirectUri { get; set; }
-
-        public TWeb3Auth.Network Network { get; set; }
-
         public Web3AuthOptions Web3AuthOptions { get; set; }
 
         public LoginParams LoginParams { get; set; }
