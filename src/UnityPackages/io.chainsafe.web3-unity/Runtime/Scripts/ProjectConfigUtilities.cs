5d6eff3240693d03244ea2df711fcd4fe6e86bc1
robGG1997 <142783680+robGG1997@users.noreply.github.com>
2023-09-12 12:46:26 +0300

    Automated unity tests 603 (#605)
    Moved Tests into package
    Automated package tests using https://game.ci/docs/github/test-runner
    GitHub action implemented for running tests
    Better tests initialization (DLLs published for tests and ProjectConfig instantiated)
    Package now testable
    Moved scripts/samples into runtime/samples to reference web3 samples without importing samples
    
    ---------
    
    Co-authored-by: Peter Kalambet <peter@kalambet.dev>

--- src/UnityPackages/io.chainsafe.web3-unity/Runtime/Scripts/ProjectConfigUtilities.cs
+++ src/UnityPackages/io.chainsafe.web3-unity/Runtime/Scripts/ProjectConfigUtilities.cs
@@ -13,6 +13,21 @@ public static class ProjectConfigUtilities
         return projectConfig ? projectConfig : null;
     }
 
+    public static ProjectConfigScriptableObject Load(string projectId, string chainId, string chain, string network,
+        string symbol, string rpc)
+    {
+        var projectConfig = ScriptableObject.CreateInstance<ProjectConfigScriptableObject>();
+
+        projectConfig.ProjectId = projectId;
+        projectConfig.ChainId = chainId;
+        projectConfig.Chain = chain;
+        projectConfig.Network = network;
+        projectConfig.Symbol = symbol;
+        projectConfig.Rpc = rpc;
+
+        return projectConfig;
+    }
+
 #if UNITY_EDITOR
     public static ProjectConfigScriptableObject CreateOrLoad()
     {
