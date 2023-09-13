86278475f5d35932de7861c8e2754117b3f771ae
Nikola Garabandic <kantagara@gmail.com>
2023-09-13 12:12:55 +0200

    Wrote Tests for ERC1155
    Fixing linter for windows.
    Fixed exception in Unity when first opening the scene (Web3Auth related)

--- lint.bat
+++ lint.bat
@@ -2,6 +2,6 @@
 
 dotnet format --verbosity=d --verify-no-changes --severity=warn .\ChainSafe.Gaming.sln --exclude .\submodules
 
-pushd src/UnitySampleProject
+pushd .\src\UnitySampleProject
 dotnet format --verbosity=d --verify-no-changes --severity=warn .\UnitySampleProject.sln
 popd 
