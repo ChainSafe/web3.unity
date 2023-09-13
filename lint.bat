f4d9744f8b3f34c0d702e9253edfda71f891cad8
Nikola Garabandic <kantagara@gmail.com>
2023-09-13 13:57:36 +0200

    Update lint.bat
    Co-authored-by: Peter Kalambet <peter@kalambet.dev>

--- lint.bat
+++ lint.bat
@@ -3,5 +3,5 @@
 dotnet format --verbosity=d --severity=warn .\ChainSafe.Gaming.sln --exclude .\submodules
 
 pushd .\src\UnitySampleProject
-dotnet format --verbosity=d --verify-no-changes --severity=warn .\UnitySampleProject.sln
+dotnet format --verbosity=d --severity=warn .\UnitySampleProject.sln
 popd 
