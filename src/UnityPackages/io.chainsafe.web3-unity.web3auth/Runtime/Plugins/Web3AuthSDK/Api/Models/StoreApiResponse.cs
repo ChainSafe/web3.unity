07763f79eac9c9c44906de2dc801f43f8bb1fbd5
Ryan Noble <ryanjnoble@gmail.com>
2023-09-14 15:21:48 +0200

    Revert again

--- src/UnityPackages/io.chainsafe.web3-unity.web3auth/Runtime/Plugins/Web3AuthSDK/Api/Models/StoreApiResponse.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class StoreApiResponse
-{
-    public string message { get; set; }
-    public bool success { get; set; }
-}
--- src/UnityPackages/io.chainsafe.web3-unity.web3auth/Runtime/Plugins/Web3AuthSDK/Types/ShareMetadata.cs
+++
@@ -1,8 +0,0 @@
-
-public class ShareMetadata
-{
-    public string iv { get; set; }
-    public string ephemPublicKey { get; set; }
-    public string ciphertext { get; set; }
-    public string mac { get; set; }
-}
