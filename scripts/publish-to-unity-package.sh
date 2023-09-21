5f0b02f9b58f49f97ced50ef012f15a6ca04da88
Peter Kalambet <peter@kalambet.dev>
2023-09-22 00:46:11 +0200

    move scripts to the common directory

--- scripts/publish-to-unity-package.sh
+++ scripts/publish-to-unity-package.sh
@@ -10,17 +10,15 @@ dotnet publish -c release -f netstandard2.1 /property:Unity=true
 
 echo Restoring non-Unity packages...
 
-pushd ../..
 dotnet restore
-popd
 
 echo Moving files to Unity package...
 
 pushd bin/release/netstandard2.1/publish
 rm Newtonsoft.Json.dll
 rm UnityEngine.dll
+rm -rf ../../../../../../Packages/io.chainsafe.web3-unity/Runtime/Libraries
 mkdir -p ../../../../../../Packages/io.chainsafe.web3-unity/Runtime/Libraries
-rm -f ../../../../../../Packages/io.chainsafe.web3-unity/Runtime/Libraries/*
 cp *.dll ../../../../../../Packages/io.chainsafe.web3-unity/Runtime/Libraries
 popd
 popd
