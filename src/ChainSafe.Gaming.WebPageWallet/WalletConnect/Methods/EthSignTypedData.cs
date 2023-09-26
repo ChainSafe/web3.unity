44e073a42c0ce47864a77d40409cf6ac56c20404
robGG1997 <robel@chainsafe.io>
2023-09-26 14:57:03 +0300

    sign typed data implemented with Wallet Connect

--- src/ChainSafe.Gaming.WebPageWallet/WalletConnect/Methods/EthSignTypedData.cs
+++ src/ChainSafe.Gaming.WebPageWallet/WalletConnect/Methods/EthSignTypedData.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using ChainSafe.Gaming.Web3.Core.Evm;
 using Nethereum.ABI.EIP712;
 using Newtonsoft.Json;
 using UnityEngine.Scripting;
@@ -10,18 +11,19 @@ namespace ChainSafe.Gaming.Wallets.WalletConnect.Methods
 {
     [RpcMethod("eth_signTypedData")]
     [RpcRequestOptions(Clock.ONE_MINUTE, 99998)]
-    public class EthSignTypedMessage<TTypedData> : List<TypedDataRequest<TTypedData>>
+    public class EthSignTypedData<TTypedData> : List<string>
     {
-        public EthSignTypedMessage(TTypedData message)
-            : base(new TypedDataRequest<TTypedData>[]
+        public EthSignTypedData(string address, SerializableDomain domain, TTypedData message)
+            : base(new string[]
             {
-                new TypedDataRequest<TTypedData>(JsonConvert.SerializeObject(MemberDescriptionFactory.GetTypesMemberDescription(typeof(TTypedData))), message),
+                address,
+                JsonConvert.SerializeObject(new TypedDataRequest<TTypedData>(MemberDescriptionFactory.GetTypesMemberDescription(typeof(TTypedData)), domain, message)),
             })
         {
         }
 
         [Preserve] // Needed for JSON.NET serialization
-        public EthSignTypedMessage()
+        public EthSignTypedData()
         {
         }
     }
--- src/ChainSafe.Gaming.WebPageWallet/WalletConnect/Methods/TypedDataRequest.cs
+++ src/ChainSafe.Gaming.WebPageWallet/WalletConnect/Methods/TypedDataRequest.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.Generic;
+using ChainSafe.Gaming.Web3.Core.Evm;
+using Nethereum.ABI.EIP712;
 using Newtonsoft.Json;
 
 namespace ChainSafe.Gaming.Wallets.WalletConnect.Methods
@@ -6,15 +9,25 @@ namespace ChainSafe.Gaming.Wallets.WalletConnect.Methods
     [Serializable]
     public struct TypedDataRequest<TTypedData>
     {
-        public TypedDataRequest(string types, TTypedData message)
+        public TypedDataRequest(Dictionary<string, MemberDescription[]> types, SerializableDomain domain, TTypedData message)
         {
             Types = types;
+
+            PrimaryType = typeof(TTypedData).Name;
+
+            Domain = domain;
 
             Message = message;
         }
 
         [JsonProperty("types")]
-        public string Types { get; private set; }
+        public Dictionary<string, MemberDescription[]> Types { get; private set; }
+
+        [JsonProperty("primaryType")]
+        public string PrimaryType { get; private set; }
+
+        [JsonProperty("domain")]
+        public SerializableDomain Domain { get; private set; }
 
         [JsonProperty("message")]
         public TTypedData Message { get; private set; }
