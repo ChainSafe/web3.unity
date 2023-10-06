47529c0fa08371778f23a3666b2fa88aed1e2a05
Nikola <kantagara@gmail.com>
2023-10-06 18:50:25 +0200

    lint for windows is fixed

--- scripts/lint.bat
+++ scripts/lint.bat
@@ -1,7 +1,10 @@
 @echo off
 
-dotnet format --verbosity=d --severity=warn ..\ChainSafe.Gaming.sln --exclude .\submodules
+pushd ..
 
-pushd ..\src\UnitySampleProject
+dotnet format --verbosity=d --severity=warn ChainSafe.Gaming.sln --exclude /submodules
+
+pushd .\src\UnitySampleProject
 dotnet format --verbosity=d --severity=warn .\UnitySampleProject.sln
 popd 
+popd
