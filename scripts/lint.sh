93acefa4c39be7e9271ea754214106ff5ef0205f
Nikola Garabandic <kantagara@gmail.com>
2023-10-06 18:52:12 +0200

    Fixing mac linter

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -7,16 +7,14 @@ if [ "$1" == "ci" ]; then edit="--verify-no-changes"; fi
 if [ "$1" == "ci" ]; then
 dotnet format --verbosity=d $edit --severity=warn ChainSafe.Gaming.sln --exclude ./submodules
 else
-dotnet format --verbosity=d $edit --severity=warn ../ChainSafe.Gaming.sln --exclude ./submodules
+pushd ..
+dotnet format --verbosity=d $edit --severity=warn ChainSafe.Gaming.sln --exclude /submodules
 fi
 
 if [ "$edit" == "" ]; then
     echo "Linting Unity Sample Project"
-    if [ "$1" == "ci" ]; then
-    pushd src/UnitySampleProject
-    else
-    pushd ../src/UnitySampleProject
-    fi
+    pushd ./src/UnitySampleProject
     dotnet format --verbosity=d --severity=warn UnitySampleProject.sln
+    popd
     popd
 fi
