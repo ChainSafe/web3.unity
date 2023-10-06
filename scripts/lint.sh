9586d701583d04ec67519020888efb2963ea5f1c
Nikola Garabandic <kantagara@gmail.com>
2023-10-06 18:14:24 +0200

    better lint.sh script

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -4,11 +4,19 @@ set -e
 edit=""
 if [ "$1" == "ci" ]; then edit="--verify-no-changes"; fi
 
+if [ "$1" == "ci" ]; then
+dotnet format --verbosity=d $edit --severity=warn ChainSafe.Gaming.sln --exclude ./submodules
+else
 dotnet format --verbosity=d $edit --severity=warn ../ChainSafe.Gaming.sln --exclude ./submodules
+fi
 
 if [ "$edit" == "" ]; then
     echo "Linting Unity Sample Project"
+    if [ "$1" == "ci" ]; then
+    pushd src/UnitySampleProject
+    else
     pushd ../src/UnitySampleProject
+    fi
     dotnet format --verbosity=d --severity=warn UnitySampleProject.sln
     popd
 fi
