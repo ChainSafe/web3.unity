33f297565ab83d318523d65e96d5289cf0f1244d
Peter Kalambet <peter@kalambet.dev>
2023-10-16 21:00:49 +0200

    remove scripts merge conflicts

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,9 +6,6 @@ pushd "$scripts_dir"/../
 git submodule update --init
 popd
 
-## if directory exists, we make check and cd here so script can work from both when bash is run from scripts and from repo root
-[ -d "scripts" ] && cd scripts
-
 # publish DLLs to unity package
 source "$scripts_dir"/publish-to-unity-package.sh
 
