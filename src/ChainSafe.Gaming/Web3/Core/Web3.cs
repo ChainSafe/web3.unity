0ffd32cf66e0856ef4f18127372dce157ee50c57
oleksandr <oleksandr@chainsafe.io>
2023-10-19 12:46:00 +0300

    Added XML comments for Web3 folder (2)

--- src/ChainSafe.Gaming/Web3/Core/Web3.cs
+++ src/ChainSafe.Gaming/Web3/Core/Web3.cs
@@ -12,8 +12,9 @@ namespace ChainSafe.Gaming.Web3
 {
     /// <summary>
     /// Facade for all Web3-related services.
+    /// Use this as an entry point to all SDK features.
     /// </summary>
-    public class Web3 : IAsyncDisposable
+    public class Web3 : IAsyncDisposable, IInitializableWeb3
     {
         private readonly ServiceProvider serviceProvider;
         private readonly IRpcProvider? rpcProvider;
@@ -36,38 +37,47 @@ namespace ChainSafe.Gaming.Web3
             ChainConfig = serviceProvider.GetRequiredService<IChainConfig>();
         }
 
+        /// <summary>
+        /// Access the <see cref="IRpcProvider"/> component bound to the Web3 instance during the build phase.
+        /// </summary>
         public IRpcProvider RpcProvider => AssertComponentAccessible(rpcProvider, nameof(RpcProvider));
 
+        /// <summary>
+        /// Access the <see cref="ISigner"/> component bound to the Web3 instance during the build phase.
+        /// </summary>
         public ISigner Signer => AssertComponentAccessible(signer, nameof(Signer));
 
+        /// <summary>
+        /// Access the <see cref="ITransactionExecutor"/> component bound to the Web3 instance during the build phase.
+        /// </summary>
         public ITransactionExecutor TransactionExecutor => AssertComponentAccessible(transactionExecutor, nameof(TransactionExecutor));
 
+        /// <summary>
+        /// Access the event service of the Web3 instance.
+        /// </summary>
         public IEvmEvents Events => AssertComponentAccessible(events, nameof(Events));
 
+        /// <summary>
+        /// Access the <see cref="IContractBuilder"/> factory.
+        /// </summary>
         public IContractBuilder ContractBuilder { get; }
 
+        /// <summary>
+        /// Access the project configuration object.
+        /// </summary>
         public IProjectConfig ProjectConfig { get; }
 
+        /// <summary>
+        /// Access the chain configuration object.
+        /// </summary>
         public IChainConfig ChainConfig { get; }
 
+        /// <summary>
+        /// Access the service provider of this Web3 instance.
+        /// Use this to get any service, that was bound to this Web3 instance
+        /// during the build phase.
+        /// </summary>
         public IServiceProvider ServiceProvider => serviceProvider;
-
-        private static T AssertComponentAccessible<T>(T? value, string propertyName)
-            where T : notnull
-        {
-            if (value == null)
-            {
-                throw new Web3Exception(
-                  $"{propertyName} is not bound. Make sure to add an implementation of {propertyName} before using it.");
-            }
-
-            // TODO: uncomment after migration complete
-            // if (!_initialized)
-            // {
-            //   throw new Web3Exception($"Can't access {propertyName}. Initialize Web3 first.");
-            // }
-            return value;
-        }
 
         async ValueTask IAsyncDisposable.DisposeAsync()
         {
@@ -75,22 +85,21 @@ namespace ChainSafe.Gaming.Web3
             GC.SuppressFinalize(this);
         }
 
-        public async ValueTask InitializeAsync()
+        async ValueTask IInitializableWeb3.InitializeAsync()
         {
-            if (initialized)
-            {
-                throw new Web3Exception("Web3 was already initialized.");
-            }
-
             foreach (var lifecycleParticipant in serviceProvider.GetServices<ILifecycleParticipant>())
             {
                 await lifecycleParticipant.WillStartAsync();
             }
 
-            // TODO: initialize other components
             initialized = true;
         }
 
+        /// <summary>
+        /// Terminate this Web3 instance together with all of its components.
+        /// </summary>
+        /// <exception cref="Web3Exception">Web3 was already terminated.</exception>
+        /// <returns>Task handle for the asynchronous process.</returns>
         public async ValueTask TerminateAsync()
         {
             if (terminated)
@@ -108,6 +117,24 @@ namespace ChainSafe.Gaming.Web3
 
             serviceProvider.Dispose();
             terminated = true;
+        }
+
+        private T AssertComponentAccessible<T>(T? value, string propertyName)
+            where T : notnull
+        {
+            if (value == null)
+            {
+                throw new Web3Exception(
+                    $"{propertyName} is not bound. Make sure to add an implementation of {propertyName} before using it.");
+            }
+
+            // TODO: uncomment after migration complete
+            if (!initialized)
+            {
+                throw new Web3Exception($"Can't access {propertyName}. Web3 instance should be initialized first.");
+            }
+
+            return value;
         }
     }
 }
