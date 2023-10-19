5a17e3a218073393b946ae625fc37a21de04a423
Nikola Garabandic <kantagara@gmail.com>
2023-10-19 15:31:38 +0200

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,10 @@
 #! /usr/bin/env sh
 
+# Get the directory of the current script
+SCRIPT_DIR="$(dirname "$0")"
+
 # clone submodules
 git submodule update --init
 
 # publish DLLs to unity package
-./scripts/publish-to-unity-package.sh
+"$SCRIPT_DIR/publish-to-unity-package.sh"
