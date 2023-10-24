ca3ef886be1724d5ee2876ab21165facda9819ec
robGG1997 <robel@chainsafe.io>
2023-10-24 09:22:56 +0300

    removed path agnostic implementation

--- scripts/duplicate-sample.sh
+++ scripts/duplicate-sample.sh
@@ -1,8 +1,4 @@
 #! /usr/bin/env sh
-scripts_dir=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-
-pushd "$scripts_dir"/../
-
 export SOURCE_DIRECTORY="$1"
 export DESTINATION_DIRECTORY="$2"
 rm -r "$DESTINATION_DIRECTORY"
