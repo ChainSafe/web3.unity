633e1e73610d084f264896e501593c3b26bf3c9c
robGG1997 <robel@chainsafe.io>
2023-10-24 10:48:32 +0300

    echos updated

--- scripts/duplicate-sample.sh
+++ scripts/duplicate-sample.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env sh
 rm -rf "$2"
-echo "$2 directory removed"
+echo "'$2' directory removed"
 mkdir -p "$2"
-echo "$2 directory created"
+echo "'$2' directory created"
 cp -r "$1"* "$2"
-echo "$1 contents copied into $2"
+echo "'$1' contents copied into '$2'"
