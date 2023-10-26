52d183ebbb3e69fb6dbe7b0b6eba7e3caf7010a4
sneakzttv <sneakzttv@users.noreply.github.com>
2023-10-26 10:59:30 +0000

    Auto-duplicate Package Samples

--- Packages/io.chainsafe.web3-unity/Samples~/Web3.Unity/Scripts/Prefabs/Unsorted/ContractSend.cs
+++ Packages/io.chainsafe.web3-unity/Samples~/Web3.Unity/Scripts/Prefabs/Unsorted/ContractSend.cs
@@ -15,7 +15,7 @@ public class ContractSend : MonoBehaviour
     // Variables
     private string method = "addTotal";
     private string abi = "[ { \"inputs\": [ { \"internalType\": \"uint8\", \"name\": \"_myArg\", \"type\": \"uint8\" } ], \"name\": \"addTotal\", \"outputs\": [], \"stateMutability\": \"nonpayable\", \"type\": \"function\" }, { \"inputs\": [], \"name\": \"myTotal\", \"outputs\": [ { \"internalType\": \"uint256\", \"name\": \"\", \"type\": \"uint256\" } ], \"stateMutability\": \"view\", \"type\": \"function\" } ]";
-    private string contractAddress = "0x7286Cf0F6E80014ea75Dbc25F545A3be90F4904F";
+    private string contractAddress = "0xC71d13c40B4fE7e2c557eBAa12A0400dd4Df76C9";
     private object[] args =
     {
         1
