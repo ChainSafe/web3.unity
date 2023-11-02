001b646d0febf060d2564a6d54aecc191912ca3f
robGG1997 <robel@chainsafe.io>
2023-11-02 15:24:48 +0300

    small revert to login scene load changes and lootboxes sample duplicate added to CI

---
+++ src/UnitySampleProject/Assets/Samples/web3.unity SDK Lootboxes/1.0.0/Chainlink Lootbox Sample/Scripts/Scene/States/LoadRewardsState.cs
@@ -0,0 +1,20 @@
+﻿namespace LootBoxes.Chainlink.Scene.States
+{
+    public class LoadRewardsState : LootBoxSceneState
+    {
+        protected override async void OnLootBoxSceneStateEnter()
+        {
+            Context.stage.Clear();
+
+            var stageItems = await Context.rewardSpawner.Spawn(Context.LastClaimedRewards);
+            if (stageItems.Count > Context.stage.CurrentMaxItems)
+            {
+                Context.stage.SetTempMaxItems(stageItems.Count);
+            }
+
+            Context.stage.SetItems(stageItems);
+            Context.stageFocus.Focus(0, immediately: true);
+            Context.animator.SetTrigger("RewardsLoaded");
+        }
+    }
+}
