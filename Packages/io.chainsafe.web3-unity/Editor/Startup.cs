d071012115cfefc58bf6cef3bcb5ef899ffdfeda
sneakzttv <sneakz420gaming@gmail.com>
2023-11-06 13:54:58 +0800

    Technical debt collection
    Fixing some issues RE: https://github.com/ChainSafe/web3.unity/issues/680

--- Packages/io.chainsafe.web3-unity/Editor/Startup.cs
+++ Packages/io.chainsafe.web3-unity/Editor/Startup.cs
@@ -28,24 +28,6 @@ namespace ChainSafe.GamingSdk.Editor
                     return;
                 }
 
-                var performSync = WebGLTemplateSync.CheckSyncStatus() switch
-                {
-                    WebGLTemplateSyncStatus.UpToDate => false,
-
-                    WebGLTemplateSyncStatus.DoesntExist =>
-                        EditorUtility.DisplayDialog("web3.unity", "Do you wish to install the web3.unity WebGL templates into your project?", "Yes", "No"),
-
-                    WebGLTemplateSyncStatus.OutOfDate =>
-                        EditorUtility.DisplayDialog("web3.unity", "The web3.unity WebGL templates in your project are out of date, would you like to update now?", "Yes", "No"),
-
-                    _ => false,
-                };
-
-                if (performSync)
-                {
-                    WebGLTemplateSync.Syncronize();
-                }
-
                 ValidateProjectID();
             };
         }
