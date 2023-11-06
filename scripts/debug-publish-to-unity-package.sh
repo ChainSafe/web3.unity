7b891ecbc4eed774baf1a380a01b68589bffb2f8
Ryan Noble <ryanjnoble@gmail.com>
2023-11-06 16:18:29 +0100

    Fixed debug script (#702)
    * Fixed debug script
    
    * Corrected directory
    
    * Re-ran scripts
    
    * Lootboxes Sample Fix [Merge Second] (#704)
    
    * Revert "automated duplicating imported samples modifications (#595)"
    
    This reverts commit a5558941827a91df51e2dd200648dcd13a1231c8.
    
    * small revert to login scene load changes and lootboxes sample duplicate added to CI
    
    * Auto-duplicate Packages Samples
    
    * Goerli check modified/optimized to be easily replaced with sepolia or other chain
    
    * Auto-duplicate Packages Samples
    
    * Logo.psd file in sprites removed
    
    * Auto-duplicate Packages Samples
    
    * Published Solution Dependencies to Package Libraries as DLLs
    
    ---------
    
    Co-authored-by: robGG1997 <robGG1997@users.noreply.github.com>
    
    * CI updates [Merge Third] - auto commits on PR then trigger checks manually by labeling PR `ready-to-merge` and/or ready_for_review (PR out of draft) (#710)
    
    * Revert "automated duplicating imported samples modifications (#595)"
    
    This reverts commit a5558941827a91df51e2dd200648dcd13a1231c8.
    
    * auto commit on PR then run tests manually on labeling with ready-to-merge
    
    * trigger checks when PR is out of draft and ready for review too
    
    * reverted auto commit actions triggers to workflow call and added a new workflow for making auto commits
    This is done to avoid a head ref error when trying to make two pushes simultaneously
    
    * new workflow for auto commiting
    
    * small typo fix
    
    * Merged main & rebuilt
    
    * Published Solution Dependencies to Package Libraries as DLLs
    
    * Update scripts/debug-publish-to-unity-package.bat
    
    * Fixed
    
    ---------
    
    Co-authored-by: robGG1997 <142783680+robGG1997@users.noreply.github.com>
    Co-authored-by: robGG1997 <robGG1997@users.noreply.github.com>
    Co-authored-by: RyRy79261 <RyRy79261@users.noreply.github.com>

--- scripts/debug-publish-to-unity-package.sh
+++ scripts/debug-publish-to-unity-package.sh
@@ -1,9 +1,9 @@
 #! /usr/bin/env sh
-
 set -e
 
 echo Building project...
 scripts_dir=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
 pushd "$scripts_dir"/../src/ChainSafe.Gaming.Unity
 
 rm -rf obj
@@ -12,19 +12,31 @@ dotnet publish -c debug -f netstandard2.1 /property:Unity=true
 
 echo Restoring non-Unity packages...
 
-pushd ../..
 dotnet restore
-popd
 
 echo Moving files to Unity package...
 
-cd bin/debug/netstandard2.1/publish
+pushd bin/debug/netstandard2.1/publish
 rm Newtonsoft.Json.dll
 rm UnityEngine.dll
+
+# Check if io.chainsafe.web3-unity.lootboxes directory exists
+if [ -d "../../../../../../Packages/io.chainsafe.web3-unity.lootboxes" ]; then
+    rm -rf ../../../../../../Packages/io.chainsafe.web3-unity.lootboxes/Chainlink/Runtime/Libraries
+    mkdir -p ../../../../../../Packages/io.chainsafe.web3-unity.lootboxes/Chainlink/Runtime/Libraries
+    cp Chainsafe.Gaming.Chainlink.dll ../../../../../../Packages/io.chainsafe.web3-unity.lootboxes/Chainlink/Runtime/Libraries
+    cp Chainsafe.Gaming.LootBoxes.Chainlink.dll ../../../../../../Packages/io.chainsafe.web3-unity.lootboxes/Chainlink/Runtime/Libraries
+fi
+
+# Delete those DLLs so they don't get copied in the next step
+rm Chainsafe.Gaming.Chainlink.dll
+rm Chainsafe.Gaming.LootBoxes.Chainlink.dll
+
 rm Microsoft.CSharp.dll
+rm -rf ../../../../../../Packages/io.chainsafe.web3-unity/Runtime/Libraries
 mkdir -p ../../../../../../Packages/io.chainsafe.web3-unity/Runtime/Libraries
-rm -f ../../../../../../Packages/io.chainsafe.web3-unity/Runtime/Libraries/*
 cp *.dll ../../../../../../Packages/io.chainsafe.web3-unity/Runtime/Libraries
 cp *.pdb ../../../../../../Packages/io.chainsafe.web3-unity/Runtime/Libraries
-
+popd
+popd
 echo Done
