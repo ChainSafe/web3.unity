863fb1478ea639cd0e535fea7d9250477a256003
robGG1997 <142783680+robGG1997@users.noreply.github.com>
2023-11-06 14:23:13 +0300

    Lootboxes Sample Fix [Merge Second] (#704)
    * Revert "automated duplicating imported samples modifications (#595)"
    
    This reverts commit a5558941827a91df51e2dd200648dcd13a1231c8.
    
    * small revert to login scene load changes and lootboxes sample duplicate added to CI
    
    * Auto-duplicate Packages Samples
    
    * Goerli check modified/optimized to be easily replaced with sepolia or other chain
    
    * Auto-duplicate Packages Samples
    
    * Logo.psd file in sprites removed
    
    * Auto-duplicate Packages Samples
    
    * Published Solution Dependencies to Package Libraries as DLLs
    
    ---------
    
    Co-authored-by: robGG1997 <robGG1997@users.noreply.github.com>

---
+++ src/UnitySampleProject/Assets/Samples/web3.unity SDK Lootboxes/1.0.0/Chainlink Lootbox Sample/Scripts/Scene/States/FadeState.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace LootBoxes.Chainlink.Scene.States
+{
+    public enum FadeGoal
+    {
+        FadeIn,
+        FadeOut
+    }
+
+    public class FadeState : LootBoxSceneState
+    {
+        public FadeGoal Goal = FadeGoal.FadeIn;
+        public float timeScale = 1f;
+        public bool Await = true;
+
+        protected override async void OnLootBoxSceneStateEnter()
+        {
+            var task = LaunchFade();
+            if (Await)
+            {
+                await task;
+            }
+
+            Context.animator.SetTrigger("Next");
+        }
+
+        private Task LaunchFade()
+        {
+            return Goal switch
+            {
+                FadeGoal.FadeIn => Context.blackout.FadeIn(timeScale),
+                FadeGoal.FadeOut => Context.blackout.FadeOut(timeScale),
+                _ => throw new ArgumentOutOfRangeException(nameof(Goal), Goal, null)
+            };
+        }
+    }
+}
