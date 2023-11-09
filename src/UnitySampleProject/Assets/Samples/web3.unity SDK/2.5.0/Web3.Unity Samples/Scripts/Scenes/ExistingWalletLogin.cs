882ff14853124ddf37b851f45024bbf4ebc90d12
robGG1997 <robel@chainsafe.io>
2023-11-09 10:50:28 +0300

    setup for IOS build based WC's feedback

--- src/UnitySampleProject/Assets/Samples/web3.unity SDK/2.5.0/Web3.Unity Samples/Scripts/Scenes/ExistingWalletLogin.cs
+++ src/UnitySampleProject/Assets/Samples/web3.unity SDK/2.5.0/Web3.Unity Samples/Scripts/Scenes/ExistingWalletLogin.cs
@@ -14,11 +14,16 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.Assertions;
 using UnityEngine.Networking;
+using UnityEngine.Scripting;
 using UnityEngine.Serialization;
 using UnityEngine.UI;
 using WalletConnectSharp.Core;
+using WalletConnectSharp.Core.Controllers;
+using WalletConnectSharp.Events;
+using WalletConnectSharp.Events.Model;
 using WalletConnectSharp.Sign.Models;
 using WalletConnectSharp.Sign.Models.Engine;
+using WalletConnectSharp.Sign.Models.Engine.Methods;
 
 /// <summary>
 /// Login using an existing wallet using Wallet Connect.
@@ -266,4 +271,22 @@ public class ExistingWalletLogin : Login
 
         Debug.Log($"{session.Topic} Approved");
     }
+    
+#if !UNITY_MONO
+    [Preserve]
+    void SetupAOT()
+    {
+        // Reference all required models
+        // This is required so AOT code is generated for these generic functions
+        var historyFactory = new JsonRpcHistoryFactory(null);
+        Debug.Log(historyFactory.JsonRpcHistoryOfType<SessionPropose, SessionProposeResponse>().GetType().FullName);
+        Debug.Log(historyFactory.JsonRpcHistoryOfType<SessionSettle, Boolean>().GetType().FullName);
+        Debug.Log(historyFactory.JsonRpcHistoryOfType<SessionUpdate, Boolean>().GetType().FullName);
+        Debug.Log(historyFactory.JsonRpcHistoryOfType<SessionExtend, Boolean>().GetType().FullName);
+        Debug.Log(historyFactory.JsonRpcHistoryOfType<SessionDelete, Boolean>().GetType().FullName);
+        Debug.Log(historyFactory.JsonRpcHistoryOfType<SessionPing, Boolean>().GetType().FullName);
+        EventManager<string, GenericEvent<string>>.InstanceOf(null).PropagateEvent(null, null);
+        throw new InvalidOperationException("This method is only for AOT code generation.");
+    }
+#endif
 }
