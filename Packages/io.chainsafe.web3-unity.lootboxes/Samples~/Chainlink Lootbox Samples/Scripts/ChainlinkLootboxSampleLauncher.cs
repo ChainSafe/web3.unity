5344549fe688646fa986bf8eeed607221929550f
sneakzttv <sneakz420gaming@gmail.com>
2023-11-10 09:55:02 +0800

    Initialize
    Initialize

--- Packages/io.chainsafe.web3-unity.lootboxes/Samples~/Chainlink Lootbox Samples/Scripts/ChainlinkLootboxSampleLauncher.cs
+++ Packages/io.chainsafe.web3-unity.lootboxes/Samples~/Chainlink Lootbox Samples/Scripts/ChainlinkLootboxSampleLauncher.cs
@@ -18,6 +18,14 @@ namespace LootBoxes.Chainlink
     /// </summary>
     public class ChainlinkLootboxSampleLauncher : MonoBehaviour
     {
+        // Lootbox alternate chain contracts
+        // 0xF899Bc1f149030c3912f1Cb473A7b2db0ed9cE5f - Sepolia Testnet LootboxFactory
+        // 0xfa97aCce8E4929e6d13a7c418BfbA0311e9D3Bfd - Mumbai Testnet LootboxFactory
+        // 0xfa97aCce8E4929e6d13a7c418BfbA0311e9D3Bfd - Binance Testnet LootboxFactory
+        // 0xfa97aCce8E4929e6d13a7c418BfbA0311e9D3Bfd - Fuji Testnet LootboxFactory
+        // 0xfa97aCce8E4929e6d13a7c418BfbA0311e9D3Bfd - Fantom Testnet LootboxFactory
+        // 0xfa97aCce8E4929e6d13a7c418BfbA0311e9D3Bfd - Arbitrum Goerli Testnet LootboxFactory
+        // 0x1993e2dD323B5dcBd8b52dB7d370bC36D280424B - Anvil
         public LootBoxScene lootBoxScene;
         public string ContractAbi;
 
@@ -26,10 +34,10 @@ namespace LootBoxes.Chainlink
         private class Web3Config : ICompleteProjectConfig
         {
             public string ProjectId => string.Empty;
-            public string ChainId => "31337";
-            public string Chain => "Anvil";
-            public string Network => "GoChain Testnet";
-            public string Rpc => $"http://127.0.0.1:8545";
+            public string ChainId => "11155111";
+            public string Chain => "Ethereum";
+            public string Network => "Sepolia";
+            public string Rpc => $"https://sepolia.infura.io/v3/287318045c6e455ab34b81d6bcd7a65f";
             public string Ipc { get; }
             public string Ws { get; }
         }
@@ -41,10 +49,11 @@ namespace LootBoxes.Chainlink
                 {
                     services.UseUnityEnvironment();
                     services.UseRpcProvider();
-                    services.Debug().UseJsonRpcWallet(new JsonRpcWalletConfig { AccountIndex = 2 });
+                    //Debug
+                    //services.Debug().UseJsonRpcWallet(new JsonRpcWalletConfig { AccountIndex = 2 });
                     services.UseChainlinkLootboxService(new LootboxServiceConfig
                     {
-                        ContractAddress = "0x1993e2dD323B5dcBd8b52dB7d370bC36D280424B",
+                        ContractAddress = "0xF899Bc1f149030c3912f1Cb473A7b2db0ed9cE5f",
                         ContractAbi = ContractAbi
                     });
                     services.AddSingleton<Erc1155MetaDataReader>();
