90354883d480ef45a08daab89b1ff3dfec02d8c5
Nikola Garabandic <kantagara@gmail.com>
2023-11-23 13:09:06 +0100

    Updated analytics and package.json to 2.5.4 release. (#758)
    * Revert "automated duplicating imported samples modifications (#595)"
    
    This reverts commit a5558941827a91df51e2dd200648dcd13a1231c8.
    
    * Updated analytics and package.json to 2.5.4 release.
    
    * Published Solution Dependencies to Package Libraries as DLLs
    
    * revert from main
    
    * Published Solution Dependencies to Package Libraries as DLLs
    
    ---------
    
    Co-authored-by: robGG1997 <robel@chainsafe.io>
    Co-authored-by: robGG1997 <robGG1997@users.noreply.github.com>

--- src/ChainSafe.Gaming/Web3/Core/Analytics/ApiAnalyticsClient.cs
+++ src/ChainSafe.Gaming/Web3/Core/Analytics/ApiAnalyticsClient.cs
@@ -18,7 +18,7 @@ namespace ChainSafe.Gaming.Web3.Analytics
             this.projectConfig = projectConfig;
         }
 
-        public string AnalyticsVersion => "2.5.3";
+        public string AnalyticsVersion => "2.5.4";
 
         public async void CaptureEvent(AnalyticsEvent eventData)
         {
