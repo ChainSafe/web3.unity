78d5ac40a3e9019752994a02464473a6beda47a3
Nikola Garabandic <kantagara@gmail.com>
2024-01-22 10:58:37 +0100

    Making the ramp sample functional.

--- src/UnitySampleProject/Assets/Samples/web3.unity SDK Ramp/1.0.0/Web3.Unity Ramp Samples/RampSample.cs
+++ src/UnitySampleProject/Assets/Samples/web3.unity SDK Ramp/1.0.0/Web3.Unity Ramp Samples/RampSample.cs
@@ -10,41 +10,22 @@ namespace ChainSafe.Gaming.Exchangers.Ramp.Sample
 {
     public class RampSample : MonoBehaviour
     {
-        public RampExchangerConfigSO Config;
         public Button OnRampButton;
         public Button OffRampButton;
         public Button OnRampOffRampButton;
-
-        private Web3.Web3 web3;
-    
-        private async void Awake()
-        {
-            // todo figure out how to handle login when other packages' samples were not loaded
-            web3 = await new Web3Builder(ProjectConfigUtilities.Load())
-                .Configure(services =>
-                {
-                    services.UseUnityEnvironment();
-                    services.UseRpcProvider();
-                    services.UseWebGLWallet(); // todo use WalletConnect for other platforms (for all platforms probably)
-                    services.UseRampExchanger(Config);
-                }).LaunchAsync();
         
+        private void Awake()
+        {
             // Subscribe to buttons
             OnRampButton.onClick.AddListener(OnRampPressed);
             OffRampButton.onClick.AddListener(OffRampPressed);
             OnRampOffRampButton.onClick.AddListener(OnRampOffRampPressed);
-        
-            // Subscribe to Ramp events
-            web3.RampExchanger().OnRampPurchaseCreated += data 
-                => Debug.Log($"On-Ramp purchase created {data.CryptoAmount} {data.Asset.Name}");
-            web3.RampExchanger().OffRampSaleCreated += data
-                => Debug.Log($"Off-Ramp sale created {data.Fiat.Amount:C} {data.Fiat.CurrencySymbol}");
         }
 
         private async void OnRampPressed()
         {
             // Show "Buy Crypto" widget
-            var purchaseData = await web3.RampExchanger().BuyCrypto(
+            var purchaseData = await Web3Accessor.Web3.RampExchanger().BuyCrypto(
                 new RampBuyWidgetSettings
                 {
                     // For more info on widget settings check https://docs.ramp.network/configuration
@@ -65,7 +46,7 @@ namespace ChainSafe.Gaming.Exchangers.Ramp.Sample
         private async void OffRampPressed()
         {
             // Show "Sell Crypto" widget
-            var saleData = await web3.RampExchanger().SellCrypto(
+            var saleData = await Web3Accessor.Web3.RampExchanger().SellCrypto(
                 new RampSellWidgetSettings
                 {
                     // For more info on widget settings check https://docs.ramp.network/configuration
@@ -84,7 +65,7 @@ namespace ChainSafe.Gaming.Exchangers.Ramp.Sample
         private async void OnRampOffRampPressed()
         {
             // Show "Buy or Sell Crypto" widget
-            var rampTransactionData = await web3.RampExchanger().BuyOrSellCrypto(
+            var rampTransactionData = await Web3Accessor.Web3.RampExchanger().BuyOrSellCrypto(
                 new RampBuyOrSellWidgetSettings
                 {
                     // For more info on widget settings check https://docs.ramp.network/configuration 
