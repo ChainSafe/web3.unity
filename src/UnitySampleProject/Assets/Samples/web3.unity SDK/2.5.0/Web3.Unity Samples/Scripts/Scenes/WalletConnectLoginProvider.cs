41305f8669e55b86b1c44eb3ec1c748801437473
Nikola Garabandic <kantagara@gmail.com>
2024-02-01 08:16:12 +0100

    Fixed parsing error.

--- src/UnitySampleProject/Assets/Samples/web3.unity SDK/2.5.0/Web3.Unity Samples/Scripts/Scenes/WalletConnectLoginProvider.cs
+++ src/UnitySampleProject/Assets/Samples/web3.unity SDK/2.5.0/Web3.Unity Samples/Scripts/Scenes/WalletConnectLoginProvider.cs
@@ -10,6 +10,7 @@ using ChainSafe.Gaming.WalletConnect;
 using ChainSafe.Gaming.WalletConnect.Models;
 using ChainSafe.Gaming.Web3.Build;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Scenes;
 using TMPro;
 using UnityEngine;
@@ -244,7 +245,19 @@ public class WalletConnectLoginProvider : LoginProvider, IWeb3BuilderServiceAdap
             }
             else
             {
-                Debug.Log($"Fetched Supported Wallets.");
+                var json = webRequest.downloadHandler.text;
+                
+                JObject jObject = JObject.Parse(json);
+                
+    
+                supportedWallets = JsonConvert.DeserializeObject<Dictionary<string, WalletConnectWalletModel>>(jObject["listings"].ToString());
+
+                // make sure supported wallet is also supported on platform
+                supportedWallets = supportedWallets
+                    .Where(w => w.Value.IsAvailableForPlatform(UnityOperatingSystemMediator.GetCurrentPlatform()))
+                    .ToDictionary(p => p.Key, p => p.Value);
+
+                Debug.Log($"Fetched {supportedWallets.Count} Supported Wallets.");
             }
         }
     }
