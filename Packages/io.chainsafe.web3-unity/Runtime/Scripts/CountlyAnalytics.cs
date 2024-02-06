d2519a34bdcf9f76db0dd49f7bf0561ff2e7381d
Nikola Garabandic <kantagara@gmail.com>
2024-02-06 10:51:02 +0100

    Adding Editor Utils

--- Packages/io.chainsafe.web3-unity/Runtime/Scripts/CountlyAnalytics.cs
+++ Packages/io.chainsafe.web3-unity/Runtime/Scripts/CountlyAnalytics.cs
@@ -14,6 +14,7 @@ public class CountlyAnalytics : IAnalyticsClient
     
     public async void CaptureEvent(AnalyticsEvent eventData)
     {
+        Debug.Log($"Capturing event: {eventData.EventName}");
         await Countly.Instance.Events.RecordEventAsync(eventData.EventName, new Dictionary<string, object>()
         {
             { "chain", eventData.ChainId},
