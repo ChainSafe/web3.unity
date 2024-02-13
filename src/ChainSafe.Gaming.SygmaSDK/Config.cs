b0758a033501919ebe58adfa70a0145f07ce57d9
Peter Kalambet <peter@kalambet.dev>
2024-02-13 10:51:31 +0530

    Initial commit

---
+++ src/ChainSafe.Gaming.SygmaSDK/Config.cs
@@ -0,0 +1,12 @@
+﻿using ChainSafe.Gaming.Web3;
+using ChainSafe.Gaming.Web3.Build;
+
+namespace ChainSafe.Gaming.SygmaSDK
+{
+    public class Config
+    {
+        public Config(Web3Builder)
+        {
+        }
+    }
+}
