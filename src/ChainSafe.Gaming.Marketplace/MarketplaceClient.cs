173122cc1920d419737d899ff74b47bc47701cbf
oleksandr <59798783+creeppak@users.noreply.github.com>
2024-03-06 13:40:58 +0000

    Added query parameters to MarketplaceClient.cs

--- src/ChainSafe.Gaming.Marketplace/MarketplaceClient.cs
+++ src/ChainSafe.Gaming.Marketplace/MarketplaceClient.cs
@@ -1,4 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
 using ChainSafe.Gaming.Web3;
 using ChainSafe.Gaming.Web3.Environment;
 
@@ -14,14 +17,16 @@ namespace ChainSafe.Gaming.Marketplace
         private readonly IMarketplaceConfig config;
         private readonly IHttpClient httpClient;
         private IProjectConfig projectConfig;
+        private IChainConfig chainConfig;
 
-        public MarketplaceClient(IHttpClient httpClient, IProjectConfig projectConfig)
-            : this(httpClient, projectConfig, MarketplaceConfig.Default)
+        public MarketplaceClient(IHttpClient httpClient, IProjectConfig projectConfig, IChainConfig chainConfig)
+            : this(httpClient, projectConfig, chainConfig, MarketplaceConfig.Default)
         {
         }
 
-        public MarketplaceClient(IHttpClient httpClient, IProjectConfig projectConfig, IMarketplaceConfig config)
+        public MarketplaceClient(IHttpClient httpClient, IProjectConfig projectConfig, IChainConfig chainConfig, IMarketplaceConfig config)
         {
+            this.chainConfig = chainConfig;
             this.httpClient = httpClient;
             this.config = config;
             this.projectConfig = projectConfig;
@@ -32,7 +37,7 @@ namespace ChainSafe.Gaming.Marketplace
             return LoadPage(prevPage?.Cursor ?? null);
         }
 
-        public async Task<MarketplacePage> LoadPage(string? cursor = null)
+        public async Task<MarketplacePage> LoadPage(string? cursor)
         {
             var endpoint = config.EndpointOverride;
             if (endpoint.EndsWith('/'))
@@ -41,7 +46,22 @@ namespace ChainSafe.Gaming.Marketplace
             }
 
             var projectId = projectConfig.ProjectId;
-            var uri = $"{endpoint}/v1/projects/{projectId}/items";
+            var baseUri = $"{endpoint}/v1/projects/{projectId}/items";
+            var queryParameters = new Dictionary<string, string>
+            {
+                ["chainId"] = chainConfig.ChainId,
+            };
+
+            if (!string.IsNullOrEmpty(cursor))
+            {
+                queryParameters["cursor"] = cursor;
+            }
+
+            var queryParametersString = queryParameters
+                .Select(p => $"{p.Key}={HttpUtility.UrlEncode(p.Value)}");
+            var query = string.Join('&', queryParametersString);
+            var uri = $"{baseUri}?{query}";
+
             var response = await httpClient.Get<MarketplacePage>(uri);
             return response.AssertSuccess();
         }
