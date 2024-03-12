6740c28048ca988304cccaeee0e44e587c3c4ff9
sneakzttv <57473220+sneakzttv@users.noreply.github.com>
2024-03-13 04:02:44 +0800

    linter fix (#902)
    * Update Contract.cs
    
    * Update Contract.cs
    
    * Update Contract.cs
    
    * Published Solution Dependencies to Package Libraries as DLLs
    
    * Docuementation
    
    * Published Solution Dependencies to Package Libraries as DLLs
    
    * Update
    
    * Published Solution Dependencies to Package Libraries as DLLs
    
    * Update
    
    * Update MarketplaceItemStatus.cs
    
    * Published Solution Dependencies to Package Libraries as DLLs
    
    * Update
    
    * Published Solution Dependencies to Package Libraries as DLLs
    
    * Update
    
    * Published Solution Dependencies to Package Libraries as DLLs
    
    * Update
    
    Update
    
    * linter fix
    
    linter fix
    
    * Published Solution Dependencies to Package Libraries as DLLs
    
    ---------
    
    Co-authored-by: sneakzttv <sneakzttv@users.noreply.github.com>

--- src/ChainSafe.Gaming.Marketplace/Dto/MarketplaceItemToken.cs
+++ src/ChainSafe.Gaming.Marketplace/Dto/MarketplaceItemToken.cs
@@ -7,20 +7,38 @@ namespace ChainSafe.Gaming.Marketplace
     using System.Collections.Generic;
     using Newtonsoft.Json;
 
+    /// <summary>
+    /// Marketplace item token.
+    /// </summary>
     public class MarketplaceItemToken
     {
+        /// <summary>
+        /// Gets or sets token Id.
+        /// </summary>
         [JsonProperty(PropertyName = "token_id")]
         public string Id { get; set; }
 
+        /// <summary>
+        /// Gets or sets token type.
+        /// </summary>
         [JsonProperty(PropertyName = "token_type")]
         public string Type { get; set; }
 
+        /// <summary>
+        /// Gets or sets contract address.
+        /// </summary>
         [JsonProperty(PropertyName = "contract_address")]
         public string ContractAddress { get; set; }
 
+        /// <summary>
+        /// Gets or sets uri.
+        /// </summary>
         [JsonProperty(PropertyName = "uri")]
         public string Uri { get; set; }
 
+        /// <summary>
+        /// Gets or sets metadata.
+        /// </summary>
         [JsonProperty(PropertyName = "metadata")]
         public Dictionary<string, string> Metadata { get; set; }
     }
