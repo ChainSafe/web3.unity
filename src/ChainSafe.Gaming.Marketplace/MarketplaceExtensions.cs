8d5b5662222db429ad7fd05a45eb6c8d2456f32a
sneakzttv <57473220+sneakzttv@users.noreply.github.com>
2024-03-14 00:18:07 +0800

    Docstrings (#907)
    * Docstrings
    
    * Published Solution Dependencies to Package Libraries as DLLs
    
    ---------
    
    Co-authored-by: sneakzttv <sneakzttv@users.noreply.github.com>

--- src/ChainSafe.Gaming.Marketplace/MarketplaceExtensions.cs
+++ src/ChainSafe.Gaming.Marketplace/MarketplaceExtensions.cs
@@ -1,11 +1,25 @@
-﻿using ChainSafe.Gaming.Web3;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
+﻿// <copyright file="MarketplaceExtensions.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace ChainSafe.Gaming.Marketplace
 {
+    using ChainSafe.Gaming.Web3;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
+
+    /// <summary>
+    /// Marketplace extensions.
+    /// </summary>
     public static class MarketplaceExtensions
     {
+        /// <summary>
+        /// Checks if the metadata propery exists.
+        /// </summary>
+        /// <typeparam name="T">Generic type to be agnostic.</typeparam>
+        /// <param name="token">Token name.</param>
+        /// <param name="name">Name of the property.</param>
+        /// <returns>Bool true or false.</returns>
         public static bool HasMetadataProperty<T>(this MarketplaceItemToken token, string name)
         {
             if (!token.Metadata.ContainsKey(name))
@@ -25,6 +39,14 @@ namespace ChainSafe.Gaming.Marketplace
             return true;
         }
 
+        /// <summary>
+        /// Gets metadata properties.
+        /// </summary>
+        /// <typeparam name="T">Generic type to be agnostic.</typeparam>
+        /// <param name="token">Token.</param>
+        /// <param name="name">Name.</param>
+        /// <returns>Metadata property value.</returns>
+        /// <exception cref="Web3Exception">Metadata properly can't be deserialized.</exception>
         public static T GetMetadataProperty<T>(this MarketplaceItemToken token, string name)
         {
             var rawValue = token.Metadata[name];
