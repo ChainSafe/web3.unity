a3d0dfd2de753dfa32af961bf580ed2066f5b726
oleksandrchainsafe <127766834+oleksandrchainsafe@users.noreply.github.com>
2024-03-13 14:01:47 +0000

    Added "Purchase" functionality to MarketplaceClient (#899)
    * Added "Purchase" functionality to MarketplaceClient
    Added AbiHelper to read ABI directly from project resources
    + introduced lil hack in WalletConnectTransactionExecutor
    
    * Published Solution Dependencies to Package Libraries as DLLs
    
    * Added support for custom metadata properties to Marketplace items
    
    ---------
    
    Co-authored-by: oleksandr <59798783+creeppak@users.noreply.github.com>
    Co-authored-by: oleksandrchainsafe <oleksandrchainsafe@users.noreply.github.com>

--- src/ChainSafe.Gaming.Marketplace/MarketplaceClient.cs
+++ src/ChainSafe.Gaming.Marketplace/MarketplaceClient.cs
@@ -1,85 +1,62 @@
-﻿// <copyright file="MarketplaceClient.cs" company="PlaceholderCompany">
-// Copyright (c) PlaceholderCompany. All rights reserved.
-// </copyright>
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Threading.Tasks;
+using System.Web;
+using ChainSafe.Gaming.Evm.Contracts;
+using ChainSafe.Gaming.Evm.Transactions;
+using ChainSafe.Gaming.Web3;
+using ChainSafe.Gaming.Web3.Environment;
+using Nethereum.Hex.HexTypes;
 
 namespace ChainSafe.Gaming.Marketplace
 {
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Threading.Tasks;
-    using System.Web;
-    using ChainSafe.Gaming.Web3;
-    using ChainSafe.Gaming.Web3.Environment;
-
     /*
      * todo add method wrappers for "Get Marketplace Items" and "Get Item" also
      * https://docs.gaming.chainsafe.io/marketplace-api/docs/marketplaceapi/#tag/Items/operation/getItem
      */
 
-    /// <summary>
-    /// Marketplace client.
-    /// </summary>
     public class MarketplaceClient
     {
         private readonly IMarketplaceConfig config;
         private readonly IHttpClient httpClient;
         private IProjectConfig projectConfig;
         private IChainConfig chainConfig;
+        private IContractBuilder contractBuilder;
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="MarketplaceClient"/> class.
-        /// </summary>
-        /// <param name="httpClient">The http client.</param>
-        /// <param name="projectConfig">Project config.</param>
-        /// <param name="chainConfig">Chain config.</param>
-        public MarketplaceClient(IHttpClient httpClient, IProjectConfig projectConfig, IChainConfig chainConfig)
-            : this(httpClient, projectConfig, chainConfig, MarketplaceConfig.Default)
+        public MarketplaceClient(IHttpClient httpClient, IProjectConfig projectConfig, IChainConfig chainConfig, IContractBuilder contractBuilder)
+            : this(httpClient, projectConfig, chainConfig, contractBuilder, MarketplaceConfig.Default)
         {
         }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="MarketplaceClient"/> class.
-        /// </summary>
-        /// <param name="httpClient">The http client.</param>
-        /// <param name="projectConfig">Project config.</param>
-        /// <param name="chainConfig">Chain config.</param>
-        /// <param name="config">Config.</param>
-        public MarketplaceClient(IHttpClient httpClient, IProjectConfig projectConfig, IChainConfig chainConfig, IMarketplaceConfig config)
+        public MarketplaceClient(IHttpClient httpClient, IProjectConfig projectConfig, IChainConfig chainConfig,
+            IContractBuilder contractBuilder, IMarketplaceConfig config)
         {
+            this.contractBuilder = contractBuilder;
             this.chainConfig = chainConfig;
             this.httpClient = httpClient;
             this.config = config;
             this.projectConfig = projectConfig;
         }
 
-        /// <summary>
-        /// Loads the marketplace page.
-        /// </summary>
-        /// <param name="prevPage">The previous page.</param>
-        /// <returns>Loaded marketplace page by previous page.</returns>
         public Task<MarketplacePage> LoadPage(MarketplacePage? prevPage = null)
         {
-            return this.LoadPage(prevPage?.Cursor ?? null);
+            return LoadPage(prevPage?.Cursor ?? null);
         }
 
-        /// <summary>
-        /// Loads the marketplace page.
-        /// </summary>
-        /// <param name="cursor">Cursor of the page.</param>
-        /// <returns>Loaded marketplace page with cursor.</returns>
         public async Task<MarketplacePage> LoadPage(string? cursor)
         {
-            var endpoint = this.config.EndpointOverride;
+            var endpoint = config.EndpointOverride;
             if (endpoint.EndsWith('/'))
             {
                 endpoint = endpoint.Substring(0, endpoint.LastIndexOf('/'));
             }
 
-            var projectId = this.projectConfig.ProjectId;
+            var projectId = projectConfig.ProjectId;
             var baseUri = $"{endpoint}/v1/projects/{projectId}/items";
             var queryParameters = new Dictionary<string, string>
             {
-                ["chainId"] = this.chainConfig.ChainId,
+                ["chainId"] = chainConfig.ChainId,
             };
 
             if (!string.IsNullOrEmpty(cursor))
@@ -92,8 +69,22 @@ namespace ChainSafe.Gaming.Marketplace
             var query = string.Join('&', queryParametersString);
             var uri = $"{baseUri}?{query}";
 
-            var response = await this.httpClient.Get<MarketplacePage>(uri);
+            var response = await httpClient.Get<MarketplacePage>(uri);
             return response.AssertSuccess();
+        }
+
+        public Task Purchase(string marketplaceContract, string itemId, string itemPrice)
+        {
+            var itemIdInt = BigInteger.Parse(itemId);
+            var priceInt = BigInteger.Parse(itemPrice);
+            return Purchase(marketplaceContract, itemIdInt, priceInt);
+        }
+
+        public async Task Purchase(string marketplaceContract, BigInteger itemId, BigInteger itemPrice)
+        {
+            var contract = contractBuilder.Build(config.MarketplaceContractAbi, marketplaceContract);
+            var transactionPrototype = new TransactionRequest { Value = new HexBigInteger(itemPrice) };
+            await contract.Send("purchaseItem", new object[] { itemId }, transactionPrototype);
         }
     }
 }
