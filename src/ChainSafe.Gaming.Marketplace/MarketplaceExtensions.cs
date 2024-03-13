77b4970fb29763962de488020a5e101beddba626
creeppak <creeppak@gmail.com>
2024-03-13 14:01:47 +0000

    Added "Purchase" functionality to MarketplaceClient (#899)
    * Added "Purchase" functionality to MarketplaceClient
    Added AbiHelper to read ABI directly from project resources
    + introduced lil hack in WalletConnectTransactionExecutor
    
    * Published Solution Dependencies to Package Libraries as DLLs
    
    * Added support for custom metadata properties to Marketplace items
    
    ---------
    
    Co-authored-by: oleksandr <59798783+creeppak@users.noreply.github.com>
    Co-authored-by: oleksandrchainsafe <oleksandrchainsafe@users.noreply.github.com>

---
+++ src/ChainSafe.Gaming.Marketplace/MarketplaceExtensions.cs
@@ -0,0 +1,51 @@
+﻿using ChainSafe.Gaming.Web3;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ChainSafe.Gaming.Marketplace
+{
+    public static class MarketplaceExtensions
+    {
+        public static bool HasMetadataProperty<T>(this MarketplaceItemToken token, string name)
+        {
+            if (!token.Metadata.ContainsKey(name))
+            {
+                return false;
+            }
+
+            try
+            {
+                token.GetMetadataProperty<T>(name);
+            }
+            catch (Web3Exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static T GetMetadataProperty<T>(this MarketplaceItemToken token, string name)
+        {
+            var rawValue = token.Metadata[name];
+
+            if (rawValue is JObject jObjValue)
+            {
+                return jObjValue.ToObject<T>();
+            }
+
+            if (rawValue is JArray jArrayValue)
+            {
+                return jArrayValue.ToObject<T>();
+            }
+
+            if (rawValue is string)
+            {
+                return (T)rawValue;
+            }
+
+            throw new Web3Exception(
+                $"Can't deserialize metadata property with name \"{name}\" to type {typeof(T).Name}");
+        }
+    }
+}
