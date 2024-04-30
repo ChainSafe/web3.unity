ba1d2fb8d7782b3a9d5baec145b9d5467d210e35
sneakzttv <sneakzttv@users.noreply.github.com>
2024-04-30 13:52:24 +0000

    Auto Commit [skip ci] - Duplicate Packages Samples - Publish DLL Dependencies

--- Packages/io.chainsafe.web3-unity/Samples~/Web3.Unity/Scripts/Utilities/DisableGameObjectIfServiceNotActive.cs
+++ Packages/io.chainsafe.web3-unity/Samples~/Web3.Unity/Scripts/Utilities/DisableGameObjectIfServiceNotActive.cs
@@ -30,4 +30,6 @@ public class DisableGameObjectIfServiceNotActive : MonoBehaviour
     {
         gameObject.SetActive(Web3Accessor.Web3.ServiceProvider.GetService(_typesDictionary[serviceType]) != null);
     }
+
+
 }
