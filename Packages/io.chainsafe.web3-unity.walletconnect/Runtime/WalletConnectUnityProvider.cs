6277bbd97d7b24df5df61de871df8bc1a66dbcee
oleksandr <59798783+creeppak@users.noreply.github.com>
2024-05-06 16:58:48 +0100

    Added new package for WalletConnectUnity integration Extracted existing WalletConnect integration into its own package

---
+++ Packages/io.chainsafe.web3-unity.walletconnect/Runtime/WalletConnectUnityProvider.cs
@@ -0,0 +1,70 @@
+﻿using System.Threading.Tasks;
+using ChainSafe.Gaming.WalletConnect;
+using ChainSafe.Gaming.Web3;
+using ChainSafe.Gaming.Web3.Build;
+using ChainSafe.Gaming.Web3.Core;
+using WalletConnectUnity.Modal;
+using OriginalWc = WalletConnectUnity.Core.WalletConnect;
+
+namespace ChainSafe.Gaming.WalletConnectUnity
+{
+    public class WalletConnectUnityProvider : IWalletConnectProvider, ILifecycleParticipant
+    {
+        private static bool instanceStarted;
+        
+        public ValueTask WillStartAsync()
+        {
+            if (instanceStarted)
+            {
+                throw new Web3Exception(
+                    $"One instance of {nameof(WalletConnectUnityProvider)} is already running. This integration of WalletConnect does not support running multiple instances.");
+            }
+
+            if (!WalletConnectModal.IsReady)
+            {
+                throw new Web3Exception(
+                    $"{nameof(WalletConnectModal)} is not ready yet. Make sure it's ready before launching {nameof(Web3.Web3)}");
+            }
+
+            instanceStarted = true;
+            return new ValueTask(Task.CompletedTask);
+        }
+
+        public ValueTask WillStopAsync()
+        {
+            instanceStarted = false;
+
+            return new ValueTask(Task.CompletedTask);
+        }
+        
+        public Task<string> Connect()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task Disconnect()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<string> Request<T>(T data, long? expiry = null)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class WalletConnectUnityConfig
+    {
+        
+    }
+
+    public static class WalletConnectUnityExtensions
+    {
+        public static IWeb3ServiceCollection UseWalletConnectUnity(this IWeb3ServiceCollection services)
+        {
+            services.AssertServiceNotBound<IWalletConnectProvider>();
+            services.AddSingleton<IWalletConnectProvider, ILifecycleParticipant, WalletConnectUnityProvider>();
+            return services;
+        }
+    }
+}
--- src/UnitySampleProject/Assets/Samples/web3.unity SDK/2.5.0/Web3.Unity Samples/Scripts/Scenes/WalletConnectLoginProvider.cs
+++ src/UnitySampleProject/Assets/Samples/web3.unity SDK/2.5.0/Web3.Unity Samples/Scripts/Scenes/WalletConnectLoginProvider.cs
@@ -8,48 +8,48 @@ using UnityEngine.UI;
 /// <summary>
 /// Login using an existing wallet with WalletConnect.
 /// </summary>
-public class WalletConnectLoginProvider : LoginProvider, IWeb3BuilderServiceAdapter
-{
-    [SerializeField] private WalletConnectConfigSO walletConnectConfig;
-    [SerializeField] private bool autoLoginPreviousSession = true;
-    [Header("UI")]
-    [SerializeField] private Toggle rememberSessionToggle;
-    [SerializeField] private Button loginButton;
-
-    private bool storedSessionAvailable;
-
-    protected override async void Initialize()
-    {
-        base.Initialize();
-
-        await using (var lightWeb3 = await WalletConnectWeb3.BuildLightweightWeb3(walletConnectConfig))
-        {
-            storedSessionAvailable = lightWeb3.WalletConnect().ConnectionHelper().StoredSessionAvailable;
-        }
-
-        if (autoLoginPreviousSession && storedSessionAvailable) // auto-login
-        {
-            Debug.Log("Proceeding with auto-login.");
-            await TryLogin();
-        }
-
-        loginButton.onClick.AddListener(OnLoginClicked);
-    }
-
-    public Web3Builder ConfigureServices(Web3Builder web3Builder)
-    {
-        return web3Builder.Configure(services =>
-        {
-            var rememberSession = rememberSessionToggle.isOn || storedSessionAvailable;
-
-            services.UseWalletConnect(walletConnectConfig.WithRememberSession(rememberSession))
-                .UseWalletConnectSigner()
-                .UseWalletConnectTransactionExecutor();
-        });
-    }
-
-    private async void OnLoginClicked()
-    {
-        await TryLogin();
-    }
-}
+// public class WalletConnectLoginProvider : LoginProvider, IWeb3BuilderServiceAdapter // TODO: recreate in the respectful package; remove LoginProvider hierarchy
+// {
+//     [SerializeField] private WalletConnectConfigSO walletConnectConfig;
+//     [SerializeField] private bool autoLoginPreviousSession = true;
+//     [Header("UI")]
+//     [SerializeField] private Toggle rememberSessionToggle;
+//     [SerializeField] private Button loginButton;
+//
+//     private bool storedSessionAvailable;
+//
+//     protected override async void Initialize()
+//     {
+//         base.Initialize();
+//
+//         await using (var lightWeb3 = await WalletConnectWeb3.BuildLightweightWeb3(walletConnectConfig))
+//         {
+//             storedSessionAvailable = lightWeb3.WalletConnect().ConnectionHelper().StoredSessionAvailable;
+//         }
+//
+//         if (autoLoginPreviousSession && storedSessionAvailable) // auto-login
+//         {
+//             Debug.Log("Proceeding with auto-login.");
+//             await TryLogin();
+//         }
+//
+//         loginButton.onClick.AddListener(OnLoginClicked);
+//     }
+//
+//     public Web3Builder ConfigureServices(Web3Builder web3Builder)
+//     {
+//         return web3Builder.Configure(services =>
+//         {
+//             var rememberSession = rememberSessionToggle.isOn || storedSessionAvailable;
+//
+//             services.UseWalletConnect(walletConnectConfig.WithRememberSession(rememberSession))
+//                 .UseWalletConnectSigner()
+//                 .UseWalletConnectTransactionExecutor();
+//         });
+//     }
+//
+//     private async void OnLoginClicked()
+//     {
+//         await TryLogin();
+//     }
+// }
