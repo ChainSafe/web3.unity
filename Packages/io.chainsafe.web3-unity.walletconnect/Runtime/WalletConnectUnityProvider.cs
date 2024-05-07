7e553b7af8ab54e96d4ccc1ee84aebfb5cab40e9
oleksandr <59798783+creeppak@users.noreply.github.com>
2024-05-07 12:15:29 +0100

    WalletConnectUnityProvider.cs WIP

--- Packages/io.chainsafe.web3-unity.walletconnect/Runtime/WalletConnectUnityProvider.cs
+++ Packages/io.chainsafe.web3-unity.walletconnect/Runtime/WalletConnectUnityProvider.cs
@@ -1,8 +1,12 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using ChainSafe.Gaming.WalletConnect;
 using ChainSafe.Gaming.Web3;
 using ChainSafe.Gaming.Web3.Build;
 using ChainSafe.Gaming.Web3.Core;
+using WalletConnectSharp.Sign.Models;
+using WalletConnectSharp.Sign.Models.Engine;
+using WalletConnectUnity.Core;
 using WalletConnectUnity.Modal;
 using OriginalWc = WalletConnectUnity.Core.WalletConnect;
 
@@ -12,7 +16,16 @@ namespace ChainSafe.Gaming.WalletConnectUnity
     {
         private static bool instanceStarted;
         
-        public ValueTask WillStartAsync()
+        private readonly IWalletConnectUnityConfig config;
+        private readonly IChainConfig chainConfig;
+
+        public WalletConnectUnityProvider(IWalletConnectUnityConfig config, IChainConfig chainConfig)
+        {
+            this.chainConfig = chainConfig;
+            this.config = config;
+        }
+        
+        public async ValueTask WillStartAsync()
         {
             if (instanceStarted)
             {
@@ -23,11 +36,10 @@ namespace ChainSafe.Gaming.WalletConnectUnity
             if (!WalletConnectModal.IsReady)
             {
                 throw new Web3Exception(
-                    $"{nameof(WalletConnectModal)} is not ready yet. Make sure it's ready before launching {nameof(Web3.Web3)}");
+                    $"{nameof(WalletConnectModal)} is not ready yet. Make sure it's loaded and initialized before launching {nameof(Web3.Web3)}");
             }
 
             instanceStarted = true;
-            return new ValueTask(Task.CompletedTask);
         }
 
         public ValueTask WillStopAsync()
@@ -37,25 +49,92 @@ namespace ChainSafe.Gaming.WalletConnectUnity
             return new ValueTask(Task.CompletedTask);
         }
         
-        public Task<string> Connect()
+        public async Task<string> Connect()
         {
-            throw new System.NotImplementedException();
+            if (OriginalWc.Instance.IsConnected)
+            {
+                // return OriginalWc.Instance.ActiveSession todo
+            }
+
+            var tcs = new TaskCompletionSource<byte>();
+            var connectionTask = tcs.Task;
+            
+            WalletConnectModal.Connected += OnConnected;
+            WalletConnectModal.ConnectionError += ConnectionError;
+
+            try
+            {
+                WalletConnectModal.Open(new WalletConnectModalOptions
+                {
+                    IncludedWalletIds = config.IncludedWalletIds,
+                    ExcludedWalletIds = config.ExcludedWalletIds,
+                    ConnectOptions = BuildConnectOptions()
+                });
+
+                await connectionTask;
+            }
+            finally
+            {
+                WalletConnectModal.Connected -= OnConnected;
+                WalletConnectModal.ConnectionError -= ConnectionError;
+            }
+
+            void OnConnected(object sender, EventArgs e)
+            {
+                tcs.SetResult(0);
+            }
+
+            void ConnectionError(object sender, EventArgs e)
+            {
+                tcs.SetException(new Web3Exception("Error occured when tried to connect using WalletConnect."));
+            }
         }
 
         public Task Disconnect()
         {
-            throw new System.NotImplementedException();
+            WalletConnectModal.Disconnect();
+            return Task.CompletedTask;
         }
 
         public Task<string> Request<T>(T data, long? expiry = null)
         {
-            throw new System.NotImplementedException();
+            return OriginalWc.Instance.RequestAsync<T, string>(data);
+        }
+
+        private ConnectOptions BuildConnectOptions()
+        {
+            var requiredNamespaces = new RequiredNamespaces
+            {
+                {
+                    ChainConstants.Namespaces.Evm,
+                    new ProposedNamespace
+                    {
+                        Chains = new[] { $"{ChainConstants.Namespaces.Evm}:{chainConfig.ChainId}" },
+                        Events = new[] { "chainChanged", "accountsChanged" },
+                        Methods = new[]
+                        {
+                            "eth_sendTransaction",
+                            "eth_signTransaction",
+                            "eth_sign",
+                            "personal_sign",
+                            "eth_signTypedData",
+                        },
+                    }
+                },
+            };
+
+            return new ConnectOptions
+            {
+                RequiredNamespaces = requiredNamespaces
+            };
         }
     }
 
-    public class WalletConnectUnityConfig
+    public interface IWalletConnectUnityConfig
     {
-        
+        public string[] IncludedWalletIds { get; }
+
+        public string[] ExcludedWalletIds { get; }
     }
 
     public static class WalletConnectUnityExtensions
