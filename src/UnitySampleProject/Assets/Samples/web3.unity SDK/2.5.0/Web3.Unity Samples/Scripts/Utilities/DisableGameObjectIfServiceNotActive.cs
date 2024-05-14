60b9671180dacafa73556a8515a2d0422ac6ea3a
Nikola Garabandic <kantagara@gmail.com>
2024-05-14 16:41:21 +0200

    Fixing ramp popup showing up even tho ramp itself is not being used.

--- src/UnitySampleProject/Assets/Samples/web3.unity SDK/2.5.0/Web3.Unity Samples/Scripts/Utilities/DisableGameObjectIfServiceNotActive.cs
+++ src/UnitySampleProject/Assets/Samples/web3.unity SDK/2.5.0/Web3.Unity Samples/Scripts/Utilities/DisableGameObjectIfServiceNotActive.cs
@@ -7,11 +7,9 @@ using UnityEngine;
 
 public enum ServiceType
 {
-    #if RAMP_AVAILABLE
-    Ramp,
-    #endif
-    Gelato,
-    Multicall
+    Ramp = 0,
+    Gelato = 1,
+    Multicall = 2
 }
 
 public class DisableGameObjectIfServiceNotActive : MonoBehaviour
@@ -19,20 +17,27 @@ public class DisableGameObjectIfServiceNotActive : MonoBehaviour
     [SerializeField] private ServiceType serviceType;
     private readonly Dictionary<ServiceType, Type> _typesDictionary = new()
     {
-        #if RAMP_AVAILABLE
+#if RAMP_AVAILABLE
         {ServiceType.Ramp, typeof(ChainSafe.Gaming.Exchangers.Ramp.IRampExchanger)},
-        #endif
+#endif
         {ServiceType.Gelato, typeof(IGelato)},
         {ServiceType.Multicall, typeof(IMultiCall)}
     };
 
     private void Awake()
     {
-        if (_typesDictionary.TryGetValue(serviceType, out var value))
-        {
-            gameObject.SetActive(Web3Accessor.Web3.ServiceProvider.GetService(value) != null);
-        }
+        if (serviceType != ServiceType.Ramp)
+            gameObject.SetActive(Web3Accessor.Web3.ServiceProvider.GetService(_typesDictionary[serviceType]) != null);
+        else
+            CheckRamp();
     }
 
-
+    private void CheckRamp()
+    {
+#if RAMP_AVAILABLE
+            gameObject.SetActive(Web3Accessor.Web3.ServiceProvider.GetService(_typesDictionary[serviceType]) != null);
+#else 
+        gameObject.SetActive(false);
+#endif
+    }
 }
