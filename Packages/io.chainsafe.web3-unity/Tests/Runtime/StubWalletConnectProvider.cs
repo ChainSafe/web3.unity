ffc4f1ce1ada7175c5d098a8a5083f53e660ebb4
rob1997 <robelgetnetrg@gmail.com>
2024-05-22 15:35:28 +0300

    fixed tests compile error

--- Packages/io.chainsafe.web3-unity/Tests/Runtime/StubWalletConnectProvider.cs
+++ Packages/io.chainsafe.web3-unity/Tests/Runtime/StubWalletConnectProvider.cs
@@ -1,31 +1,34 @@
 ﻿using System.Threading.Tasks;
+using ChainSafe.Gaming.Evm;
 using ChainSafe.Gaming.WalletConnect;
+using ChainSafe.Gaming.Web3.Evm.Wallet;
 
 namespace Tests.Runtime
 {
-    public class StubWalletConnectProvider : IWalletConnectProvider
+    public class StubWalletConnectProvider : WalletProvider
     {
         private readonly StubWalletConnectProviderConfig config;
 
-        public StubWalletConnectProvider(StubWalletConnectProviderConfig config)
+        public StubWalletConnectProvider(StubWalletConnectProviderConfig config, ChainRegistryProvider chainRegistryProvider) : base(chainRegistryProvider: chainRegistryProvider)
         {
             this.config = config;
         }
 
-        public Task<string> Connect()
+        public override Task<string> Connect()
         {
             return Task.FromResult(config.WalletAddress);
         }
 
-        public Task Disconnect()
+        public override Task Disconnect()
         {
             // empty
             return Task.CompletedTask;
         }
 
-        public Task<string> Request<T>(T data, long? expiry = null)
+        public override Task<T> Perform<T>(string method, params object[] parameters)
         {
-            return Task.FromResult(config.StubResponse);
+            // box and unbox the stub response.
+            return Task.FromResult((T) (config.StubResponse as object));
         }
     }
 }
