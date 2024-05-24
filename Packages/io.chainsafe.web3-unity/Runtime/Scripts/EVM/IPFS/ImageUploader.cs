f416522be98ee1be270626db5e51803b20ee3ecc
sneakzttv <57473220+sneakzttv@users.noreply.github.com>
2024-05-24 23:49:10 +0800

    IPFS/Collections multi-platform image upload fixes (#969)
    * Platform fixes
    
    Platform fixes
    
    * Update IPFS.cs
    
    * Update IPFS.cs
    
    * Cleanup
    
    Cleanup
    
    * test
    
    test
    
    * Purchase function
    
    Purchase function
    
    * Cleanup
    
    Cleanup
    
    * Web3auth fix
    
    * Update UploadPlatforms.cs
    
    * Delete Plugins.meta
    
    * Fixes
    
    Fixes
    
    * Update FilePicker.mm
    
    * Update UploadPlatforms.cs
    
    * Update UploadPlatforms.cs

--- Packages/io.chainsafe.web3-unity/Runtime/Scripts/EVM/IPFS/ImageUploader.cs
+++ Packages/io.chainsafe.web3-unity/Runtime/Scripts/EVM/IPFS/ImageUploader.cs
@@ -5,13 +5,15 @@ namespace ChainSafe.Gaming
 {
     public class ImageUploader : MonoBehaviour
     {
+        #if UNITY_WEBGL && !UNITY_EDITOR
         /// <summary>
         /// Emits an event with image data for uploading with webgl
         /// </summary>
         /// <param name="imageData">Image data</param>
         public void OnImageSelected(string imageData)
         {
-            CSServer.OnImageSelected(imageData);
+            UploadPlatforms.OnImageSelected(imageData);
         }
+        #endif
     }
 }
