949b13dd071ef39a59ffa22c3b7ee75cbb1c2191
rob1997 <robelgetnetrg@gmail.com>
2024-06-05 13:57:50 +0300

    merge fixes

--- src/ChainSafe.Gaming.HyperPlay/HyperPlayConfig.cs
+++ src/ChainSafe.Gaming.HyperPlay/HyperPlayConfig.cs
@@ -5,6 +5,10 @@ namespace ChainSafe.Gaming.HyperPlay
     /// </summary>
     public class HyperPlayConfig : IHyperPlayConfig
     {
+        public string SignMessageRpcMethodName => "personal_sign";
+
+        public string SignTypedMessageRpcMethodName => "eth_signTypedData_v4";
+
         /// <summary>
         /// Remember the HyperPlay session.
         /// Like remember me for login.
