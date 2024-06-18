2da811050ab9dc4c3d51b56e9fc057fff59f9938
sneakzttv <57473220+sneakzttv@users.noreply.github.com>
2024-06-19 06:06:12 +0800

    Update ClipboardManager.m

--- Packages/io.chainsafe.web3-unity.web3auth/Runtime/Plugins/Web3AuthSDK/iOS/ClipboardManager.m
+++ Packages/io.chainsafe.web3-unity.web3auth/Runtime/Plugins/Web3AuthSDK/iOS/ClipboardManager.m
@@ -1,15 +1,13 @@
 ﻿#import <UIKit/UIKit.h>
 
-extern "C" {
-    void CopyToClipboard(const char* text) {
-        NSString *textToCopy = [NSString stringWithUTF8String:text];
-        UIPasteboard *pasteboard = [UIPasteboard generalPasteboard];
-        pasteboard.string = textToCopy;
-    }
+void CopyToClipboard(const char* text) {
+    NSString *textToCopy = [NSString stringWithUTF8String:text];
+    UIPasteboard *pasteboard = [UIPasteboard generalPasteboard];
+    pasteboard.string = textToCopy;
+}
 
-    const char* PasteFromClipboard() {
-        UIPasteboard *pasteboard = [UIPasteboard generalPasteboard];
-        NSString *pastedText = pasteboard.string;
-        return pastedText.UTF8String;
-    }
+const char* PasteFromClipboard() {
+    UIPasteboard *pasteboard = [UIPasteboard generalPasteboard];
+    NSString *pastedText = pasteboard.string;
+    return [pastedText UTF8String];
 }
