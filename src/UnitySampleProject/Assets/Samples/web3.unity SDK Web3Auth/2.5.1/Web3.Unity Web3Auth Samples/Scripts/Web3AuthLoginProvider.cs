a1d304b55c96227df5b3fce1061a257c7c41af7d
sneakzttv <57473220+sneakzttv@users.noreply.github.com>
2024-06-24 23:50:48 +0800

    Revert "fix"
    This reverts commit 7f313b4e030d58d91c0516583dfc41b8a020c51d.

--- src/UnitySampleProject/Assets/Samples/web3.unity SDK Web3Auth/2.5.1/Web3.Unity Web3Auth Samples/Scripts/Web3AuthLoginProvider.cs
+++ src/UnitySampleProject/Assets/Samples/web3.unity SDK Web3Auth/2.5.1/Web3.Unity Web3Auth Samples/Scripts/Web3AuthLoginProvider.cs
@@ -1,13 +1,11 @@
 using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;
 using ChainSafe.Gaming.UnityPackage;
 using ChainSafe.Gaming.UnityPackage.Common;
 using ChainSafe.Gaming.Web3.Analytics;
 using ChainSafe.Gaming.Web3.Build;
 using ChainSafe.GamingSdk.Web3Auth;
 using Scenes;
-using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using Network = Web3Auth.Network;
@@ -34,22 +32,11 @@ public class Web3AuthLoginProvider : LoginProvider, IWeb3BuilderServiceAdapter
     [SerializeField] private Network network;
     [Header("UI")]
     [SerializeField] private List<ProviderAndButtonPair> providerAndButtonPairs;
-    [Header("Wallet GUI Options")]
-    [SerializeField] private GameObject web3AuthWalletGUIPrefab;
-    [SerializeField] private bool enableWalletGUI;
-    [SerializeField] private bool displayWalletIcon;
-    [SerializeField] private bool autoConfirmTransactions;
-    [SerializeField] private bool autoPopUpWalletOnTx;
-    [SerializeField] private Sprite walletIcon;
-    [SerializeField] private Sprite walletLogo;
-    [SerializeField] public TMP_FontAsset displayFont;
-    [SerializeField] private Color primaryBackgroundColour;
-    [SerializeField] private Color menuBackgroundColour;
-    [SerializeField] private Color primaryTextColour;
-    [SerializeField] private Color secondaryTextColour;
-    [SerializeField] private Color borderButtonColour;
+
     private bool useProvider;
+
     private Provider selectedProvider;
+
     private bool rememberMe;
 
     public void SetRememberMe(bool rememberMe)
@@ -72,12 +59,15 @@ public class Web3AuthLoginProvider : LoginProvider, IWeb3BuilderServiceAdapter
         if (!string.IsNullOrEmpty(uri.Fragment))
         {
             useProvider = false;
+
             await TryLogin();
+           
         }
 #else
-        if (!string.IsNullOrEmpty(KeyStoreManagerUtils.getPreferencesData(KeyStoreManagerUtils.SESSION_ID)) && rememberMe)
+        if (!string.IsNullOrEmpty(KeyStoreManagerUtils.getPreferencesData(KeyStoreManagerUtils.SESSION_ID)))
         {
             useProvider = false;
+            rememberMe = true;
             await TryLogin();
             Debug.Log("Restoring existing Web3Auth session (Remember Me");
         }
@@ -91,43 +81,9 @@ public class Web3AuthLoginProvider : LoginProvider, IWeb3BuilderServiceAdapter
             useProvider = true;
         }
         selectedProvider = provider;
+
         await TryLogin();
         LogAnalytics(provider);
-    }
-    
-    public override async Task TryLogin()
-    {
-        try
-        {
-            await (this as ILoginProvider).Login();
-            EnableWalletGUI();
-        }
-        catch (Exception e)
-        {
-            Debug.LogError($"Login failed, please try again\n{e.Message} (see console for more details)");
-            throw;
-        }
-    }
-
-    private void EnableWalletGUI()
-    {
-        if (!enableWalletGUI) return;
-        var w3aWalletGuiConfig = new Web3AuthWalletGUI.Web3AuthWalletConfig
-        {
-            DisplayWalletIcon = displayWalletIcon,
-            AutoPopUpWalletOnTx = autoPopUpWalletOnTx,
-            AutoConfirmTransactions = autoConfirmTransactions,
-            WalletIcon = walletIcon,
-            WalletLogo = walletLogo,
-            DisplayFont = displayFont,
-            PrimaryBackgroundColour = primaryBackgroundColour,
-            MenuBackgroundColour = menuBackgroundColour,
-            PrimaryTextColour = primaryTextColour,
-            SecondaryTextColour = secondaryTextColour,
-            BorderButtonColour = borderButtonColour
-        };
-        var web3AuthWalletInstance = Instantiate(web3AuthWalletGUIPrefab);
-        web3AuthWalletInstance.GetComponent<Web3AuthWalletGUI>().Initialize(w3aWalletGuiConfig);
     }
 
     private void LogAnalytics(Provider provider)
