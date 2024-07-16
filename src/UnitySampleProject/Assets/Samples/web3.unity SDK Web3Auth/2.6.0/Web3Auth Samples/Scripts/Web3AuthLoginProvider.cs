bb4aa191ecc4317c18d0ebd830422360ffc8e06a
rob1997 <robelgetnetrg@gmail.com>
2024-07-16 16:27:39 +0300

    build error fix

--- src/UnitySampleProject/Assets/Samples/web3.unity SDK Web3Auth/2.6.0/Web3Auth Samples/Scripts/Web3AuthLoginProvider.cs
+++ src/UnitySampleProject/Assets/Samples/web3.unity SDK Web3Auth/2.6.0/Web3Auth Samples/Scripts/Web3AuthLoginProvider.cs
@@ -78,44 +78,44 @@ public class Web3AuthLoginProvider : ConnectionHandler, IWeb3BuilderServiceAdapt
         base.Initialize();
         providerAndButtonPairs.ForEach(p =>
             p.Button.onClick.AddListener(delegate { LoginWithWeb3Auth(p.Provider); }));
-#if !UNITY_EDITOR && UNITY_WEBGL
-        Web3AuthWebGLConnected += Web3AuthSet;
-        var projectSettings = ProjectConfigUtilities.Load();
-        SetLoginCallback(Web3AuthConnected);
-        //1155 is a decimal number, we need to convert it to an integer
-        InitWeb3Auth(clientId, new HexBigInteger(BigInteger.Parse(projectSettings.ChainId)).HexValue, 
-        projectSettings.Rpc, projectSettings.Network, "", projectSettings.Symbol, "", network.ToString().ToLower());
-#else
+// #if !UNITY_EDITOR && UNITY_WEBGL
+//         Web3AuthWebGLConnected += Web3AuthSet;
+//         var projectSettings = ProjectConfigUtilities.Load();
+//         SetLoginCallback(Web3AuthConnected);
+//         //1155 is a decimal number, we need to convert it to an integer
+//         InitWeb3Auth(clientId, new HexBigInteger(BigInteger.Parse(projectSettings.ChainId)).HexValue, 
+//         projectSettings.Rpc, projectSettings.Network, "", projectSettings.Symbol, "", network.ToString().ToLower());
+// #else
         if (!string.IsNullOrEmpty(KeyStoreManagerUtils.getPreferencesData(KeyStoreManagerUtils.SESSION_ID)))
         {
             rememberMe = true;
             await TryConnect();
             Debug.Log("Restoring existing Web3Auth session (Remember Me");
         }
-#endif
+// #endif
     }
-#if !UNITY_EDITOR && UNITY_WEBGL
-    private async void Web3AuthSet(string sessionId)
-    {
-        Web3AuthWebGLConnected -= Web3AuthSet;
-        KeyStoreManagerUtils.savePreferenceData(KeyStoreManagerUtils.SESSION_ID, sessionId);
-        await TryConnect();
-    }
-#endif
-
-#if !UNITY_EDITOR && UNITY_WEBGL
-    [MonoPInvokeCallback(typeof(Action))]
-    private static void Web3AuthConnected(string sessionId)
-    {
-        Web3AuthWebGLConnected?.Invoke(sessionId);
-    }
-#endif
+// #if !UNITY_EDITOR && UNITY_WEBGL
+//     private async void Web3AuthSet(string sessionId)
+//     {
+//         Web3AuthWebGLConnected -= Web3AuthSet;
+//         KeyStoreManagerUtils.savePreferenceData(KeyStoreManagerUtils.SESSION_ID, sessionId);
+//         await TryConnect();
+//     }
+// #endif
+//
+// #if !UNITY_EDITOR && UNITY_WEBGL
+//     [MonoPInvokeCallback(typeof(Action))]
+//     private static void Web3AuthConnected(string sessionId)
+//     {
+//         Web3AuthWebGLConnected?.Invoke(sessionId);
+//     }
+// #endif
 
     private async void LoginWithWeb3Auth(Provider provider)
     {
-#if UNITY_WEBGL && !UNITY_EDITOR
-        Web3AuthLogin(provider.ToString().ToLower(), rememberMe);
-#else
+// #if UNITY_WEBGL && !UNITY_EDITOR
+//         Web3AuthLogin(provider.ToString().ToLower(), rememberMe);
+// #else
         selectedProvider = provider;
         await TryConnect();
         LogAnalytics(provider);
@@ -157,7 +157,7 @@ public class Web3AuthLoginProvider : ConnectionHandler, IWeb3BuilderServiceAdapt
             EventName = $"Login provider {provider}",
             PackageName = "io.chainsafe.web3-unity.web3auth",
         });
-#endif
+// #endif
 
     }
 
