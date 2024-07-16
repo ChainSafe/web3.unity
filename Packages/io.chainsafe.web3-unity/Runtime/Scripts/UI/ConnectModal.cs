70f6e4923de330f6651e4c0f2da33465e4cf37f7
rob1997 <robelgetnetrg@gmail.com>
2024-07-16 17:16:11 +0300

    prefabs, sprites and ui scripts

---
+++ Packages/io.chainsafe.web3-unity/Runtime/Scripts/UI/ConnectModal.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ChainSafe.Gaming
+{
+    public class ConnectModal : MonoBehaviour
+    {
+        [SerializeField] private ErrorOverlay errorOverlay;
+        [SerializeField] private LoadingOverlay loadingOverlay;
+        [SerializeField] private Button closeButton;
+        // Closes modal when background is clicked
+        [SerializeField] private Button closeFromBackgroundButton;
+        
+        [Space]
+        
+        [SerializeField] private RectTransform providerContainer;
+
+        private void Start()
+        {
+            closeButton.onClick.AddListener(Close);
+            closeFromBackgroundButton.onClick.AddListener(Close);
+        }
+
+        public void DisplayError(string message)
+        {
+            errorOverlay.DisplayError(message);
+        }
+        
+        public void ShowLoading()
+        {
+            loadingOverlay.gameObject.SetActive(true);
+        }
+        
+        public void HideLoading()
+        {
+            loadingOverlay.gameObject.SetActive(false);
+        }
+
+        public ConnectionProvider AddProvider(ConnectionProvider provider)
+        {
+            return Instantiate(provider, providerContainer);
+        }
+        
+        private void Close()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
