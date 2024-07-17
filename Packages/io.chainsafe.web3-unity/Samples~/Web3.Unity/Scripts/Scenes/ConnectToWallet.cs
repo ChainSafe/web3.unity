9f4ee13a845a452a839291acdb0f93770f0618d6
rob1997 <robelgetnetrg@gmail.com>
2024-07-17 08:49:36 +0000

    Auto Commit [skip ci] - Duplicate Packages Samples - Publish DLL Dependencies

---
+++ Packages/io.chainsafe.web3-unity/Samples~/Web3.Unity/Scripts/Scenes/ConnectToWallet.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using ChainSafe.Gaming;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConnectToWallet : MonoBehaviour
+{
+    [SerializeField] private ConnectModal connectModalPrefab;
+    [SerializeField] private RectTransform connectModalContainer;
+    [SerializeField] private Button connectButton;
+
+    private ConnectModal _connectModalInstance;
+    
+    private void Start()
+    {
+        connectButton.onClick.AddListener(PromptConnectModal);
+    }
+
+    private void PromptConnectModal()
+    {
+        if (_connectModalInstance != null)
+        {
+            _connectModalInstance.gameObject.SetActive(true);
+            
+            return;
+        }
+        
+        _connectModalInstance = Instantiate(connectModalPrefab, connectModalContainer);
+    }
+}
