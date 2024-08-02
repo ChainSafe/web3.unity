320e40b2b299f720b353c4f30464a6c79c756513
creeppak <creeppak@gmail.com>
2024-08-02 13:08:27 +0100

    Removed the ContractExtensions.SendAndGet methods which return value, as Send methods doesn't ever return value

--- src/ChainSafe.Gaming/RPC/Contracts/ContractExtensions.cs
+++ src/ChainSafe.Gaming/RPC/Contracts/ContractExtensions.cs
@@ -23,22 +23,13 @@ namespace ChainSafe.Gaming.Evm.Contracts
             {
                 var teamId = 256;
                 var teammateAddresses = await contract.Call<List<string>, BigInteger>("getTeammateAddresses", teamId);
-                var blockedPlayerAddresses = new[] { "0x20934800uu9283798439873298dfu2f93" };
-                var allowedTeammateAddresses = teammateAddresses.Except(blockedPlayerAddresses).ToList();
             }
 
-            // block players
-            {
-                var blockAddresses = new[] { "0x20934800uu9283798439873298dfu2f93", "0x20934800uu9283798439873298dfu2f93" };
-                await Send(contract, "blockPlayers", blockAddresses);
-            }
-
-            // greet friends, read their statuses
+            // greet friends
             {
                 var friendIds = new[] { "0x20934800uu9283798439873298dfu2f93", "0x20934800uu9283798439873298dfu2f93" };
                 var onlineOnly = false;
-                var statuses = (await contract.SendAndGet<List<string>, string[], bool>("greetMany", friendIds, onlineOnly))
-                    .ToList();
+                await contract.Send("greetMany", friendIds, onlineOnly);
             }
         }
 
@@ -141,7 +132,7 @@ namespace ChainSafe.Gaming.Evm.Contracts
         }
 
         /// <summary>
-        /// Asynchronously sends a transaction to a smart contract method that is not expected to have a return value.
+        /// Asynchronously sends a transaction to a smart contract method.
         /// </summary>
         /// <param name="contract">The contract instance on which the transaction is sent.</param>
         /// <param name="methodName">The name of the smart contract method to which the transaction is sent.</param>
@@ -156,7 +147,7 @@ namespace ChainSafe.Gaming.Evm.Contracts
         }
 
         /// <summary>
-        /// Asynchronously sends a transaction to a smart contract method that is not expected to have a return value.
+        /// Asynchronously sends a transaction to a smart contract method.
         /// </summary>
         /// <typeparam name="TIn1">The type of the first input parameter.</typeparam>
         /// <param name="contract">The contract instance on which the transaction is sent.</param>
@@ -174,7 +165,7 @@ namespace ChainSafe.Gaming.Evm.Contracts
         }
 
         /// <summary>
-        /// Asynchronously sends a transaction to a smart contract method that is not expected to have a return value.
+        /// Asynchronously sends a transaction to a smart contract method.
         /// </summary>
         /// <typeparam name="TIn1">The type of the first input parameter.</typeparam>
         /// <typeparam name="TIn2">The type of the second input parameter.</typeparam>
@@ -195,7 +186,7 @@ namespace ChainSafe.Gaming.Evm.Contracts
         }
 
         /// <summary>
-        /// Asynchronously sends a transaction to a smart contract method that is not expected to have a return value.
+        /// Asynchronously sends a transaction to a smart contract method.
         /// </summary>
         /// <typeparam name="TIn1">The type of the first input parameter.</typeparam>
         /// <typeparam name="TIn2">The type of the second input parameter.</typeparam>
@@ -216,100 +207,6 @@ namespace ChainSafe.Gaming.Evm.Contracts
             TransactionRequest requestPrototype = null)
         {
             return contract.Send(methodName, new object[] { in1, in2, in3 }, requestPrototype);
-        }
-
-        /// <summary>
-        /// Asynchronously sends a transaction to a smart contract method that is expected to return a single value of a specified type.
-        /// </summary>
-        /// <typeparam name="TOut">The expected type of the return value from the smart contract method.</typeparam>
-        /// <param name="contract">The contract instance on which the transaction is sent.</param>
-        /// <param name="methodName">The name of the smart contract method to which the transaction is sent.</param>
-        /// <param name="requestPrototype">Optional. A prototype object for the transaction request. This can include transaction parameters such as gas limit, gas price, value, etc., if necessary.</param>
-        /// <returns>A task representing the asynchronous operation, with a result of the specified type <typeparamref name="TOut"/>, containing the return value from the smart contract method.</returns>
-        /// <exception cref="Web3Exception">Thrown if the response from the smart contract is empty, or if the response cannot be converted to the specified type <typeparamref name="TOut"/>.</exception>
-        public static async Task<TOut> SendAndGet<TOut>(
-            this IContract contract,
-            string methodName,
-            TransactionRequest requestPrototype = null)
-        {
-            var response = await contract.Send(methodName, null, requestPrototype);
-            AssertResponseNotEmpty(response);
-            return AssertAndConvertSingle<TOut>(response);
-        }
-
-        /// <summary>
-        /// Asynchronously sends a transaction to a smart contract method that is expected to return a single value of a specified type.
-        /// </summary>
-        /// <typeparam name="TOut">The expected type of the return value from the smart contract method.</typeparam>
-        /// <typeparam name="TIn1">The type of the first input parameter.</typeparam>
-        /// <param name="contract">The contract instance on which the transaction is sent.</param>
-        /// <param name="methodName">The name of the smart contract method to which the transaction is sent.</param>
-        /// <param name="in1">The first input parameter.</param>
-        /// <param name="requestPrototype">Optional. A prototype object for the transaction request. This can include transaction parameters such as gas limit, gas price, value, etc., if necessary.</param>
-        /// <returns>A task representing the asynchronous operation, with a result of the specified type <typeparamref name="TOut"/>, containing the return value from the smart contract method.</returns>
-        /// <exception cref="Web3Exception">Thrown if the response from the smart contract is empty, or if the response cannot be converted to the specified type <typeparamref name="TOut"/>.</exception>
-        public static async Task<TOut> SendAndGet<TOut, TIn1>(
-            this IContract contract,
-            string methodName,
-            TIn1 in1,
-            TransactionRequest requestPrototype = null)
-        {
-            var response = await contract.Send(methodName, new object[] { in1 }, requestPrototype);
-            AssertResponseNotEmpty(response);
-            return AssertAndConvertSingle<TOut>(response);
-        }
-
-        /// <summary>
-        /// Asynchronously sends a transaction to a smart contract method that is expected to return a single value of a specified type.
-        /// </summary>
-        /// <typeparam name="TOut">The expected type of the return value from the smart contract method.</typeparam>
-        /// <typeparam name="TIn1">The type of the first input parameter.</typeparam>
-        /// <typeparam name="TIn2">The type of the second input parameter.</typeparam>
-        /// <param name="contract">The contract instance on which the transaction is sent.</param>
-        /// <param name="methodName">The name of the smart contract method to which the transaction is sent.</param>
-        /// <param name="in1">The first input parameter.</param>
-        /// <param name="in2">The second input parameter.</param>
-        /// <param name="requestPrototype">Optional. A prototype object for the transaction request. This can include transaction parameters such as gas limit, gas price, value, etc., if necessary.</param>
-        /// <returns>A task representing the asynchronous operation, with a result of the specified type <typeparamref name="TOut"/>, containing the return value from the smart contract method.</returns>
-        /// <exception cref="Web3Exception">Thrown if the response from the smart contract is empty, or if the response cannot be converted to the specified type <typeparamref name="TOut"/>.</exception>
-        public static async Task<TOut> SendAndGet<TOut, TIn1, TIn2>(
-            this IContract contract,
-            string methodName,
-            TIn1 in1,
-            TIn2 in2,
-            TransactionRequest requestPrototype = null)
-        {
-            var response = await contract.Send(methodName, new object[] { in1, in2 }, requestPrototype);
-            AssertResponseNotEmpty(response);
-            return AssertAndConvertSingle<TOut>(response);
-        }
-
-        /// <summary>
-        /// Asynchronously sends a transaction to a smart contract method that is expected to return a single value of a specified type.
-        /// </summary>
-        /// <typeparam name="TOut">The expected type of the return value from the smart contract method.</typeparam>
-        /// <typeparam name="TIn1">The type of the first input parameter.</typeparam>
-        /// <typeparam name="TIn2">The type of the second input parameter.</typeparam>
-        /// <typeparam name="TIn3">The type of the third input parameter.</typeparam>
-        /// <param name="contract">The contract instance on which the transaction is sent.</param>
-        /// <param name="methodName">The name of the smart contract method to which the transaction is sent.</param>
-        /// <param name="in1">The first input parameter.</param>
-        /// <param name="in2">The second input parameter.</param>
-        /// <param name="in3">The third input parameter.</param>
-        /// <param name="requestPrototype">Optional. A prototype object for the transaction request. This can include transaction parameters such as gas limit, gas price, value, etc., if necessary.</param>
-        /// <returns>A task representing the asynchronous operation, with a result of the specified type <typeparamref name="TOut"/>, containing the return value from the smart contract method.</returns>
-        /// <exception cref="Web3Exception">Thrown if the response from the smart contract is empty, or if the response cannot be converted to the specified type <typeparamref name="TOut"/>.</exception>
-        public static async Task<TOut> SendAndGet<TOut, TIn1, TIn2, TIn3>(
-            this IContract contract,
-            string methodName,
-            TIn1 in1,
-            TIn2 in2,
-            TIn3 in3,
-            TransactionRequest requestPrototype = null)
-        {
-            var response = await contract.Send(methodName, new object[] { in1, in2, in3 }, requestPrototype);
-            AssertResponseNotEmpty(response);
-            return AssertAndConvertSingle<TOut>(response);
         }
 
         private static void AssertResponseNotEmpty(object[] response)
