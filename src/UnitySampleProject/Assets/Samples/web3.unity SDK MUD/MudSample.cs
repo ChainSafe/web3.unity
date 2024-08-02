586e1ed86f5201574fa5dbe1b890ba73cda9b879
creeppak <creeppak@gmail.com>
2024-08-02 13:21:59 +0100

    Reworked MUD integration to use our Contract system Added Record Querying without getter functions on the contract side Reworked MUD sample to reflect the changes

--- src/UnitySampleProject/Assets/Samples/web3.unity SDK MUD/MudSample.cs
+++ src/UnitySampleProject/Assets/Samples/web3.unity SDK MUD/MudSample.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using ChainSafe.Gaming.Debugging;
+using ChainSafe.Gaming.Evm.Contracts;
 using ChainSafe.Gaming.Evm.JsonRpc;
 using ChainSafe.Gaming.Mud;
 using ChainSafe.Gaming.UnityPackage;
@@ -13,6 +15,8 @@ using ChainSafe.Gaming.Web3.Evm.Wallet;
 using ChainSafe.Gaming.Web3.Unity;
 using Nethereum.ABI.FunctionEncoding.Attributes;
 using Nethereum.Contracts;
+using Nethereum.Mud;
+using TMPro;
 using UnityEditor.VersionControl;
 using UnityEngine;
 using Task = System.Threading.Tasks.Task;
@@ -20,63 +24,71 @@ using Task = System.Threading.Tasks.Task;
 public class MudSample : MonoBehaviour
 {
     public string WorldContractAddress;
+    public TextAsset WorldContractAbi;
+    public TMP_Text CounterLabel;
 
     private Web3 web3;
-    
-    [Function("app__increment", "uint32")]
-    public class IncrementFunction : FunctionMessage
-    {
-    }
-    
-    // Try implementing a new Mud System with the GetCounter function. Uncomment then:
-    
-    // [Function("app__getCounter", "uint32")]
-    // public class GetCounterFunction : FunctionMessage
-    // {
-    // }
+    private MudWorld world;
 
-    private void Awake()
+    public class CounterRecord : TableRecordSingleton<CounterRecord.CounterValue> // singleton table record - no key required
+    {
+        public class CounterValue
+        {
+            [Parameter("uint32", "value", 1)] // column name
+            public BigInteger Counter { get; set; }
+        }
+
+        public CounterRecord() : base("app", "Counter") // table name
+        {
+        }
+    }
+
+    private async void Awake()
     {
         Debug.Log("To run this sample successfully you should have the MUD tutorial project running in the background.\n" +
                   "Follow the link https://mud.dev/quickstart");
+        
+        // 1. Initialize Web3 client.
+        web3 = await new Web3Builder(ProjectConfigUtilities.Load(), ProjectConfigUtilities.BuildLocalhostConfig())
+            .Configure(services =>
+            {
+                services.UseUnityEnvironment();
+                services.UseRpcProvider();
+
+                // Initializes Wallet as the first account of the locally running Ethereum Node (Anvil).  
+                services.Debug().UseJsonRpcWallet(new JsonRpcWalletConfig { AccountIndex = 0 });
+                    
+                // Enable MUD
+                services.UseMud();
+            }).LaunchAsync();
+        Debug.Log("Web3 client ready");
+        
+        // 2. Create MUD World client.
+        world = web3.Mud().BuildWorld(WorldContractAddress, WorldContractAbi.text);
+        Debug.Log("MUD World client ready");
+
+        // 3. Query current counter value.
+        var counterValue = (await world.Query<CounterRecord, CounterRecord.CounterValue>()).Counter;
+        Debug.Log($"Counter value on load: {counterValue}");
+        CounterLabel.text = counterValue.ToString("d");
     }
 
     public async void IncrementCounter()
     {
-        if (web3 != null)
+        if (web3 is null)
         {
-            Debug.Log("Terminating old web3..");
-            await web3.TerminateAsync();
+            Debug.LogError("Can't run sample. Web3 client was not initialized.");
+            return;
         }
-
-        // 1. Initialize web3 client.
-        web3 = await new Web3Builder(ProjectConfigUtilities.Load(), ProjectConfigUtilities.BuildLocalhostConfig())
-                .Configure(services =>
-                {
-                    services.UseUnityEnvironment();
-                    services.UseRpcProvider();
-
-                    // Initializes the Wallet as the first account of the locally running Ethereum Node (Anvil).  
-                    services.Debug().UseJsonRpcWallet(new JsonRpcWalletConfig { AccountIndex = 0 });
-                    services.UseMud();
-                }).LaunchAsync();
-        Debug.Log("New Web3 client ready");
         
-        // 2. Create MUD World client.
-        var world = web3.Mud().BuildWorld(WorldContractAddress);
-        Debug.Log("MUD World client ready");
+        // 4. Send transaction to execute the Increment function of the World contract.
+        Debug.Log("Sending transaction to execute the Increment function..");
+        await world.Send("app__increment");
+        Debug.Log($"Increment successful");
         
-        // 3. Send transaction to execute the Increment function of the World contract.
-        Debug.Log("Sending transaction to execute IncrementFunction..");
-        await world.Send<IncrementFunction>();
-        Debug.Log("Increment successful");
-        
-        
-        Debug.Log("You can also call read-only functions for free. Open this script in the editor to continue.");
-        
-        // 4. Try implementing a new Mud System with the GetCounter function. Uncomment then:
-        
-        // var counter = await world.Call<GetCounterFunction, int>();
-        // Debug.Log($"Counter is \"{counter}\"");
+        // 5. Query new counter value.
+        var counterValue = (await world.Query<CounterRecord, CounterRecord.CounterValue>()).Counter;
+        Debug.Log($"Counter value after increment: {counterValue}");
+        CounterLabel.text = counterValue.ToString("d");
     }
 }
