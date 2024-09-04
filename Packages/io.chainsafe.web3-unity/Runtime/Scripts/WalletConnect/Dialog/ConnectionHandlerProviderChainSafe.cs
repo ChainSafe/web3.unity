875dfa1c504156d992a127b2f657a5782b31b4d1
rob1997 <robelgetnetrg@gmail.com>
2024-09-04 14:48:22 +0300

    reverted some removed files

---
+++ Packages/io.chainsafe.web3-unity/Runtime/Scripts/WalletConnect/Dialog/ConnectionHandlerProviderChainSafe.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using ChainSafe.Gaming.WalletConnect.Connection;
+using UnityEngine;
+
+namespace ChainSafe.Gaming.WalletConnect.Dialog
+{
+    /// <summary>
+    /// Simple version of connection handler provider.
+    /// Lacking Pool functionality and loading from Addressables.
+    /// </summary>
+    [CreateAssetMenu(menuName = "ChainSafe/WalletConnect/Connection Handler Provider", fileName = "ConnectionHandlerProvider", order = 0)]
+    public class ConnectionHandlerProviderChainSafe : ConnectionHandlerProviderSO
+    {
+        [SerializeField] private ConnectionHandlerBehaviour handlerPrefab;
+
+        private ConnectionHandlerBehaviour loadedHandler;
+
+        public override Task<IConnectionHandler> ProvideHandler()
+        {
+            if (loadedHandler != null)
+            {
+                return Task.FromResult((IConnectionHandler)loadedHandler);
+            }
+
+            loadedHandler = Instantiate(handlerPrefab);
+            return Task.FromResult((IConnectionHandler)loadedHandler);
+        }
+    }
+}
---
+++ Packages/io.chainsafe.web3-unity/Runtime/Scripts/WalletConnect/Dialog/ConnectionHandlerProviderSO.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using ChainSafe.Gaming.WalletConnect.Connection;
+using UnityEngine;
+
+namespace ChainSafe.Gaming.WalletConnect.Dialog
+{
+    public abstract class ConnectionHandlerProviderSO : ScriptableObject, IConnectionHandlerProvider
+    {
+        public abstract Task<IConnectionHandler> ProvideHandler();
+    }
+}
