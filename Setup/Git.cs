79f9a82b9869b835dbf7dd9b19412ae7034dc682
rob1997 <robelgetnetrg@gmail.com>
2024-10-15 15:38:27 +0300

    configure and push added

--- Setup/Git.cs
+++ Setup/Git.cs
@@ -2,6 +2,8 @@ namespace Setup;
 
 public static class Git
 {
+    private static bool _configured;
+    
     public static void Add(string path)
     {
         $"git add \"{path}\" -f".RunWithBash();
@@ -9,11 +11,24 @@ public static class Git
     
     public static void Commit(string message)
     {
+        if (!_configured)
+        {
+            Configure();
+        }
+        
         $"git commit -m \"{message}\"".RunWithBash();
     }
     
     public static void Push()
     {
         "git push".RunWithBash();
+    }
+
+    private static void Configure()
+    {
+        "git config user.email ${{ github.actor }}@users.noreply.github.com".RunWithBash();
+        "git config user.name ${{ github.actor }}".RunWithBash();
+
+        _configured = true;
     }
 }
