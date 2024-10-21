7d22a67c2ac2119939b74e45bdb2f1b6facda458
rob1997 <robelgetnetrg@gmail.com>
2024-10-21 18:16:03 +0300

    CI Improvements & Optimizations (#1198)
    * Revert "automated duplicating imported samples modifications (#595)"
    
    This reverts commit a5558941827a91df51e2dd200648dcd13a1231c8.
    
    * made main package testable, revert due to a mistake
    
    * initial commit to test CI
    
    * path updated
    
    * path updated for linux
    
    * Packages version changes implemented
    
    * Program renamed to Setup
    
    * checkpoint before testing git
    
    * git push added
    
    * proper logging
    
    * more log check
    
    * more log test
    
    * powershell added
    
    * added missing command line argument
    
    * reverted to run with bash
    
    * removed powershell package
    
    * exception support
    
    * configure and push added
    
    * deploy key added for push
    
    * bad substitution fix
    
    * github env vars setup
    
    * checkpoint
    
    * tags and dependency version changes
    
    * manual workflow and release added
    
    * release version from input
    
    * cool-er title for release workflow
    
    * Ignore serialization only, on some Package properties
    
    * removed unused using statement
    
    * documentation added
    
    * first draft for duplicate sample and fixed release
    
    * fixed parallel run of jobs
    
    * extra arguments for release
    Git push added as a runnable so push happens at the end
    release order changed
    
    * new trigger for testing
    
    * fixed compile error
    
    * documentation
    
    * added ref branch to commit on pull requst
    
    * removed additional arguments for release
    
    * setup works on both workflow call or form workflow dispatch
    
    * cache test
    
    * compile error fix
    
    * parallel fix
    
    * removed push at the end
    
    * sync dependencies done
    
    * added dotnet restore
    
    * removed restore added git enable property
    
    * update submodule before dotnet run
    
    * git fix
    
    * default case added for runnable args switch
    
    * release by default, git enabled by default and duplicate and sync now can also be triggered manually
    
    * cleaned up scripts
    
    * workflows updated and optimized
    
    * more workflow updates and optimizations
    
    * parallel runs (to save time)
    
    * test sync branches
    
    * fetch added
    
    * main renamed
    
    * --allow-unrelated-histories flag added
    
    * git config added
    
    * sync branches to main workflow
    
    * git back to static
    
    * reverted sample duplicate test
    
    * parallel run to save some time
    
    ---------
    
    Co-authored-by: robGG1997 <robel@chainsafe.io>

--- Setup/Git.cs
+++ Setup/Git.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Setup.Utils;
 
 namespace Setup;
@@ -8,10 +11,27 @@ namespace Setup;
 public static class Git
 {
     private static bool _configured;
+
+    public static bool Enabled { get; private set; } = true;
+
+    public static void Configure(string configuration)
+    {
+        switch (configuration)
+        {
+            case "enabled":
+                Enabled = true;
+                break;
+            case "disabled":
+                Enabled = false;
+                break;
+            default:
+                throw new Exception($"-git can't configure {configuration}");
+        }
+    }
     
     public static void Add(string path)
     {
-        $"git add \"{path}\" -f".RunWithBash();
+        $"git add \"{path}\" -f".Run();
     }
     
     public static void Commit(string message, string[] tags = null)
@@ -20,8 +40,9 @@ public static class Git
         {
             Configure();
         }
-        
-        $"git commit -m \"{message} [skip ci]\"".RunWithBash();
+
+        // Checks if there are any changes to commit before committing
+        $"git diff-index --cached --quiet HEAD || git commit -m \"{message} [skip ci]\"".Run();
 
         if (tags != null)
         {
@@ -37,7 +58,7 @@ public static class Git
     
     public static void Push(string[] tags = null)
     {
-        "git push -f".RunWithBash();
+        "git push -f".Run();
 
         if (tags != null)
         {
@@ -45,7 +66,7 @@ public static class Git
             {
                 if (!string.IsNullOrEmpty(tag))
                 {
-                    $"git push origin \"{tag}\"".RunWithBash();
+                    $"git push origin \"{tag}\"".Run();
                 }
             }
         }
@@ -60,13 +81,13 @@ public static class Git
     
     public static void Tag(string tag)
     {
-        $"git tag \"{tag}\"".RunWithBash();
+        $"git tag \"{tag}\"".Run();
     }
     
     private static void Configure()
     {
-        "git config user.email $git_email".RunWithBash();
-        "git config user.name $git_actor".RunWithBash();
+        "git config user.email $git_email".Run();
+        "git config user.name $git_actor".Run();
 
         _configured = true;
     }
