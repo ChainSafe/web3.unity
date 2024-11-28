9c61e1bb7e1b8110a7ce32b597a90073e32aac9c
rob1997 <robelgetnetrg@gmail.com>
2024-11-28 13:30:51 +0300

    setup now uses a proper command line parser

---
+++ Setup/SyncBranches.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using CommandLine;
+using Newtonsoft.Json;
+using Setup.Utils;
+
+namespace Setup;
+
+/// <summary>
+/// Sync branches (merge).
+/// </summary>
+[Verb("sync_branches", HelpText = "Sync branches.")]
+public class SyncBranches : IRunnable
+{
+    public int Order => 0;
+    
+    public IEnumerable<string> CommandLineArgument { get; set; }
+
+    private readonly string[] _excludedPaths;
+
+    [Option('b', "base", Required = true, HelpText = "Base branch.")]
+    public string Base { get; set; }
+    
+    [Option('t', "target", Required = true, HelpText = "Target branch.")]
+    public string Target { get; set; }
+    
+    [Option('s', "skip_ci", Required = false, Default = true, HelpText = "Disable/Skip CI triggers for sync/merge.")]
+    public bool SkipCi { get; set; }
+    
+    public SyncBranches()
+    {
+        Dependency[] dependencies = JsonConvert.DeserializeObject<Dependency[]>(File.ReadAllText("dependencies.json"));
+
+        _excludedPaths = Array.ConvertAll(dependencies, d => d.Path);
+    }
+    
+    public void Run()
+    {
+        Console.WriteLine($"Syncing {Target} branch to {Base} branch...");
+        
+        Git.Fetch(Base);
+        
+        Git.Checkout(Base);
+        
+        Git.Fetch(Target);
+        
+        Git.Checkout(Target);
+        
+        foreach (string path in _excludedPaths)
+        {
+            Git.Checkout(Base, path);
+            
+            Git.Add(path);
+        }
+        
+        Git.Commit("Exclude Paths - Auto Commit", skipCi: SkipCi, allowEmpty: true);
+        
+        Git.Merge(Base);
+        
+        Git.Commit($"Sync to {Base} - Auto Commit", skipCi: SkipCi, allowEmpty: true);
+        
+        Git.Push();
+        
+        Console.WriteLine($"Synced {Target} branch to {Base} branch completed.");
+    }
+}
