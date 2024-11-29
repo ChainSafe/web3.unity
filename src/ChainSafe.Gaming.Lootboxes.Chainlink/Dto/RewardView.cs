e37fa61be8e150a0631bce2ef670aaa7cfa7b8c0
sneakzttv <57473220+sneakzttv@users.noreply.github.com>
2024-11-29 16:03:44 +0800

    reward view

--- src/ChainSafe.Gaming.Lootboxes.Chainlink/Dto/RewardView.cs
+++ src/ChainSafe.Gaming.Lootboxes.Chainlink/Dto/RewardView.cs
@@ -2,25 +2,6 @@
 
 namespace ChainSafe.Gaming.Lootboxes.Chainlink
 {
-    public class ExtraRewardInfo
-    {
-        public ExtraRewardInfo(uint id, uint units, uint amountPerUnit, uint balance)
-        {
-            Id = id;
-            Units = units;
-            AmountPerUnit = amountPerUnit;
-            Balance = balance;
-        }
-
-        public uint Id { get; set; }
-
-        public uint Units { get; set; }
-
-        public uint AmountPerUnit { get; set; }
-
-        public uint Balance { get; set; }
-    }
-
     public class RewardView
     {
         public RewardView(string rewardToken, uint rewardType, uint units, uint amountPerUnit, uint balance, List<ExtraRewardInfo> extra)
@@ -44,5 +25,24 @@ namespace ChainSafe.Gaming.Lootboxes.Chainlink
         public uint Balance { get; set; }
 
         public List<ExtraRewardInfo> Extra { get; set; }
+    }
+
+    public class ExtraRewardInfo
+    {
+        public ExtraRewardInfo(uint id, uint units, uint amountPerUnit, uint balance)
+        {
+            Id = id;
+            Units = units;
+            AmountPerUnit = amountPerUnit;
+            Balance = balance;
+        }
+
+        public uint Id { get; set; }
+
+        public uint Units { get; set; }
+
+        public uint AmountPerUnit { get; set; }
+
+        public uint Balance { get; set; }
     }
 }
