d06d1211cfdc7c0927e5e3264422f37cec6566bc
Nikola Garabandic <kantagara@gmail.com>
2025-01-16 16:19:30 +0100

    Fixing comments from Rob

--- Setup/SyncDependencies.cs
+++ Setup/SyncDependencies.cs
@@ -62,12 +62,6 @@ public class SyncDependencies : IRunnable
             foreach (string name in namespaces)
             {
                 string fileSource = Path.Combine(source, $"{name}.dll");
-
-                if (!Path.Exists(fileSource))
-                {
-                    Console.WriteLine($"File {name}.dll not found");
-                    continue;
-                }
                 
                 string fileDestination = Path.Combine(destination, $"{name}.dll");
                 
